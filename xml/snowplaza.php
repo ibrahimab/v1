ee99d32020d2216396585e6b086e564029d7aaad
miguelChalet <miguel@chalet.nl>
2012-06-20 16:41:18 +0200

    xmt_text() function bijgevoegd.

--- xml/snowplaza.php
+++ xml/snowplaza.php
@@ -3,17 +3,17 @@ $unixdir="../";
 include("../admin/vars.php");
 $db->query("SELECT DISTINCT land_id, land FROM view_accommodatie WHERE atonen = '1' AND ttonen= '1' AND wzt='".addslashes($vars["seizoentype"])."'");
 if($db->num_rows()){
-	$xmloutput=header("Content-Type: text/xml; charset=ISO-8859-1");
+	$xmloutput=header("Content-Type: text/xml; charset=utf-8");
 	$xmloutput="";
-	$xmloutput.="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n";
+	$xmloutput.="<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 	$xmloutput.="<landen>\n";
 	while($db->next_record()){
 		$xmloutput.="<land>\n";
 		$xmloutput.="<landID>";
-		$xmloutput.=$db->f("land_id");
+		$xmloutput.=xml_text($db->f("land_id"));
 		$xmloutput.="</landID>\n";
 		$xmloutput.="<landNaam>";
-		$xmloutput.=$db->f("land");
+		$xmloutput.=xml_text($db->f("land"));
 		$xmloutput.="</landNaam>\n";
 		$xmloutput.="<landUrl>";
 		$xmloutput.=$vars["basehref"].txt("menu_land")."/".wt_convert2url($db->f("land"))."/";
@@ -26,10 +26,10 @@ if($db->num_rows()){
 		while($db->next_record()){
 			$xmloutput.="<skigebied>\n";
 			$xmloutput.="<skigebiedID>";
-			$xmloutput.=$db->f("skigebied_id");
+			$xmloutput.=xml_text($db->f("skigebied_id"));
 			$xmloutput.="</skigebiedID>\n";
 			$xmloutput.="<skigebiedNaam>";
-			$xmloutput.=$db->f("skigebied");
+			$xmloutput.=xml_text($db->f("skigebied"));
 			$xmloutput.="</skigebiedNaam>\n";
 			$xmloutput.="<skigebiedUrl>";
 			$xmloutput.=$vars["basehref"].txt("menu_skigebied")."/".wt_convert2url($db->f("skigebied"))."/";
@@ -43,10 +43,10 @@ if($db->num_rows()){
 			while($db->next_record()){
 				$xmloutput.="<plaats>\n";
 				$xmloutput.="<plaatsID>";
-				$xmloutput.=$db->f("plaats_id");
+				$xmloutput.=xml_text($db->f("plaats_id"));
 				$xmloutput.="</plaatsID>\n";
 				$xmloutput.="<plaatsNaam>";
-				$xmloutput.=$db->f("plaats");
+				$xmloutput.=xml_text($db->f("plaats"));
 				$xmloutput.="</plaatsNaam>\n";
 				$xmloutput.="<plaatsUrl>";
 				$xmloutput.=$vars["basehref"].txt("menu_plaats")."/".wt_convert2url($db->f("plaats"))."/";
