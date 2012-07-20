9b0e93f6819638314f060ffd3a6f51cf6da8a3c2
miguelChalet <miguel@chalet.nl>
2012-07-20 10:06:57 +0200

    in favorieten.html de printfunctie geschreven.
    de querys overal aangepast aan de nieuwe norm.
    een afbeelding toegevoegd voor de mail icoon

--- favorieten.php
+++ favorieten.php
@@ -1,12 +1,9 @@
 <?php
 
 include("admin/vars.php");
-//dat heb ik zo gedaan omdat de menu al wordt opgebouwd voordat de html pagina wordt opgeladen. wanneer deze is opgeladen gebeurt er niks meer 
-//met de menu balk. tijdens het ophalen van deze php bestand wordt de vars bestand opgehaald en kan de menubalk nog worden overschreven.
-//ik geprobeerd om deze gebeurtenis vanuit de html pagina te laten gebeuren maar zonder succes.
 if($vars["websitetype"]==1){
 	$klantfavs=array();
-		$db->query("select type_id from bezoeker_favoriet where bezoeker_id='".$_COOKIE["sch"]."'");
+		$db->query("SELECT type_id FROM bezoeker_favoriet WHERE bezoeker_id='".addslashes($_COOKIE["sch"])."';");
 		while($db->next_record()){
 			array_push($klantfavs,$db->f("type_id"));
 		}
