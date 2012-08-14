161f8f596383871f19bae7dd60b216b16ff6abef
miguelChalet <miguel@chalet.nl>
2012-08-14 17:38:42 +0200

    if statement toegevoegd om ervoor te zorgen dat geen null value wordt weergegeven

--- cms_boekingen_facturen.php
+++ cms_boekingen_facturen.php
@@ -357,9 +357,11 @@ if($form->okay) {
 			$pdf->Cell(0,4,$gegevens["stap1"]["reisbureau_naam"],0,1);
 			$pdf->Cell(0,4,$gegevens["stap1"]["reisbureau_adres"],0,1);
 			$pdf->Cell(0,4,$gegevens["stap1"]["reisbureau_postcode"]." ".$gegevens["stap1"]["reisbureau_plaats"].($gegevens["stap1"]["reisbureau_land"]<>"Nederland" ? " / ".$gegevens["stap1"]["reisbureau_land"] : ""),0,1);
-			$pdf->Ln();
-			$pdf->Ln();
-			$pdf->Cell(0,4,html("btwnummer","factuur").$gegevens["stap1"]["reisbureau_btwnummer"],0,1);
+			if($gegevens["stap1"]["reisbureau_btwnummer"]){
+				$pdf->Ln();
+				$pdf->Ln();
+				$pdf->Cell(0,4,html("btwnummer","factuur").$gegevens["stap1"]["reisbureau_btwnummer"],0,1);
+			}
 			
 		} else {
 			# NAW hoofdboeker
