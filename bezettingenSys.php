0f6791326c9ee5fb6031e6542452125f66561c54
miguelChalet <miguel@chalet.nl>
2012-08-28 16:16:11 +0200

    excel library en bezettingenSys

--- bezettingenSys.php
+++ bezettingenSys.php
@@ -1,7 +1,13 @@
 <?php
 
 //$mustlogin=true;
-$vars["verberg_linkerkolom"]=false;
+$vars["verberg_linkerkolom"]=true;
+$vars["verberg_lastacc"]=true;
+$vars["verberg_zoekenboeklinks"]=true;
+$vars["verberg_directnaar"]=true;
+$laat_titel_weg=true;
+//include("excel/excelwriter.inc.php");
+require_once("excel/Classes/PHPExcel.php");
 include("admin/vars.php");
 
 include "content/opmaak.php";
---
+++ cms_bezettingenSys.php
@@ -0,0 +1,7 @@
+<?php
+$mustlogin=true;
+require_once("excel/Classes/PHPExcel.php");
+include("admin/vars.php");
+
+$layout->display_all("");
+?>
