507f4b7e9295289c2c4a3b5b901d345ebe9b48da
miguelChalet <miguel@chalet.nl>
2012-08-30 10:53:02 +0200

    removal

--- bezettingenSys.php
+++ bezettingenSys.php
@@ -7,7 +7,7 @@ $vars["verberg_zoekenboeklinks"]=true;
 $vars["verberg_directnaar"]=true;
 $laat_titel_weg=true;
 //include("excel/excelwriter.inc.php");
-require_once("excel/Classes/PHPExcel.php");
+//require_once("excel/Classes/PHPExcel.php");
 include("admin/vars.php");
 
 include "content/opmaak.php";
