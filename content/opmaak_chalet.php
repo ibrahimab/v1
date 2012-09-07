4cec6031e6803d9757c6f144c868d39a82167f6c
Jeroen Boschman <boschman@gmail.com>
2012-09-07 08:33:00 +0200

    Verdwenen wijzigingen van mij teruggezet

--- content/opmaak_chalet.php
+++ content/opmaak_chalet.php
@@ -692,8 +692,7 @@ if($voorkant_cms and !$_GET["cmsuit"] and $interneinfo) {
 # Balk met cookie-melding
 if($vars["websiteland"]=="nl" and (!$_COOKIE["cookiemelding_gelezen"] or $_SERVER["DOCUMENT_ROOT"]=="/home/webtastic/html")) {
 	echo "<p>&nbsp;</p>";
-	echo "<div class=\"clear\"></div>";
-	echo "<div id=\"cookie_bottombar\"><div id=\"cookie_bottombar_wrapper\">Deze website maakt gebruik van cookies. Lees ons <a href=\"".$vars["path"]."privacy-statement.php?testsysteem=1\">privacy statement</a> voor meer informatie.<div id=\"cookie_bottombar_close\">sluiten</div></div></div>";
+	echo "<div id=\"cookie_bottombar\"><div id=\"cookie_bottombar_wrapper\"><div id=\"cookie_bottombar_text\">".html("cookiemelding","vars",array("h_1"=>"<a href=\"".$vars["path"]."privacy-statement.php\">","h_2"=>"</a>"))."</div><div id=\"cookie_bottombar_close\">sluiten</div></div></div>";
 }
 
 # Zorgen dat zoekenboek_overlay naar beneden schuift i.v.m. "laatst bekeken"-button
