be748fa2fe957368b5fc0734baec9541b65a0d16
miguelChalet <miguel@chalet.nl>
2012-09-18 14:13:31 +0200

    regex uitgebreid het kan nu echt niet meer stuk

--- scripts/functions_favorieten.js
+++ scripts/functions_favorieten.js
@@ -1,6 +1,6 @@
 
 function validateEmail(email) {  
-	var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	var pattern = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
 	return email.match(pattern);     
 }  
 
