f1647f721c53feb1b4972d19a7ddfa441f5f4d0b
miguelChalet <miguel@chalet.nl>
2012-10-03 12:27:21 +0200

    ideal betalingstestsysteem voor sisow

--- checkout.php
+++ checkout.php
@@ -12,7 +12,18 @@ include("admin/vars.php");
 //voorlopig eerst een demo object.
 $boekingObject=array();
 $boekingObject["klantNaam"]="Miguel Moukimou";
-$boekingObject["Totaal boeking bedrag"]=3000;
-$boekingObject["Termijn"]=1000;
+$boekingObject["Totaal boeking bedrag"]=3021;
+$boekingObject["Termijn"]=920;
+$boekingObject["reserveringsnummer"]="C12105049";
+$boekingObject["plaats"]="test plaats";
+$boekingObject["Accommodatie"]="Chalet Almhaus Peter(8-10 pers.)";
+$boekingObject["Deelnemers"]="8 personen";
+$boekingObject["Verblijfsperiode"]="16 februari 2013 - 23 februari 2013";
+$boekingObject["product"][0]="1x Accommodatie";
+$boekingObject["product"]["prijs"][0]=2810;
+$boekingObject["product"][1]="1x Energie en eindschoonmaakkosten (excl. keukenhoek) verplicht te voldoen";
+$boekingObject["product"]["prijs"][1]=191;
+$boekingObject["Reserveringskosten"]=true;
+
 include "content/opmaak.php";
 ?>
