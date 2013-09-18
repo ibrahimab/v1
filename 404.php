17a4aae8e07ccb8100cda1b80d76f45b98160128
Jeroen Boschman <jeroen@webtastic.nl>
2013-09-18 10:21:32 +0200

    redirects oude Venturasol-site

--- 404.php
+++ 404.php
@@ -10,38 +10,40 @@ if(ereg("/pic/chalets/",$_SERVER["REQUEST_URI"])) {
 } else {
 
 
-	if($_SERVER["HTTP_HOST"]=="test.venturasol.nl") {
+	if($_SERVER["HTTP_HOST"]=="www.venturasol.nl") {
 		// redirects voor Venturasol (301's oude site naar nieuwe site)
 
-		$redirect["http://test.venturasol.nl/skivakanties/home"]="http://test.venturasol.nl/";
-		$redirect["http://test.venturasol.nl/skigebieden"]="http://test.venturasol.nl/skigebieden.php";
-		$redirect["http://test.venturasol.nl/skivakanties/aanbiedingen"]="http://test.venturasol.nl/aanbiedingen.php";
-		$redirect["http://test.venturasol.nl/skivakanties/zoek-en-boek"]="http://test.venturasol.nl/zoek-en-boek.php";
-		$redirect["http://test.venturasol.nl/skivakanties/bestemming/les-trois-vallees"]="http://test.venturasol.nl/wintersport/skigebied/Les-Trois-Vallees/";
-		$redirect["http://test.venturasol.nl/voorwaarden"]="http://test.venturasol.nl/algemenevoorwaarden.php";
-		$redirect["http://test.venturasol.nl/appartementen"]="http://test.venturasol.nl/zoek-en-boek.php";
-		$redirect["http://test.venturasol.nl/ski-chalet"]="http://test.venturasol.nl/zoek-en-boek.php";
-		$redirect["http://test.venturasol.nl/skigebieden"]="http://test.venturasol.nl/skigebieden.php";
-		$redirect["http://test.venturasol.nl/offerte-winter"]="http://test.venturasol.nl/contact.php";
-		$redirect["http://test.venturasol.nl/wie"]="http://test.venturasol.nl/zoek-en-boek.php";
-		$redirect["http://test.venturasol.nl/service-winter"]="http://test.venturasol.nl/zoek-en-boek.php";
-		$redirect["http://test.venturasol.nl/zonvakanties/home"]="http://test.venturasol.nl/zoek-en-boek.php";
-		$redirect["http://test.venturasol.nl/sitemap-winter"]="http://test.venturasol.nl/zoek-en-boek.php";
-		$redirect["http://test.venturasol.nl/wie-zijn-wij"]="http://test.venturasol.nl/zoek-en-boek.php";
-		$redirect["http://test.venturasol.nl/skivakanties/alle-accommodaties"]="http://test.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/skivakanties/home"]="http://www.venturasol.nl/";
+		$redirect["http://www.venturasol.nl/skigebieden"]="http://www.venturasol.nl/skigebieden.php";
+		$redirect["http://www.venturasol.nl/skivakanties/aanbiedingen"]="http://www.venturasol.nl/aanbiedingen.php";
+		$redirect["http://www.venturasol.nl/skivakanties/zoek-en-boek"]="http://www.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/skivakanties/bestemming/les-trois-vallees"]="http://www.venturasol.nl/wintersport/skigebied/Les-Trois-Vallees/";
+		$redirect["http://www.venturasol.nl/voorwaarden"]="http://www.venturasol.nl/algemenevoorwaarden.php";
+		$redirect["http://www.venturasol.nl/appartementen"]="http://www.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/ski-chalet"]="http://www.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/skigebieden"]="http://www.venturasol.nl/skigebieden.php";
+		$redirect["http://www.venturasol.nl/offerte-winter"]="http://www.venturasol.nl/contact.php";
+		$redirect["http://www.venturasol.nl/wie"]="http://www.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/home"]="http://www.venturasol.nl/";
+		$redirect["http://www.venturasol.nl/service-winter"]="http://www.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/zonvakanties/home"]="http://www.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/sitemap-winter"]="http://www.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/wie-zijn-wij"]="http://www.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/wiezijnwij"]="http://www.venturasol.nl/zoek-en-boek.php";
+		$redirect["http://www.venturasol.nl/skivakanties/alle-accommodaties"]="http://www.venturasol.nl/zoek-en-boek.php";
 		// $redirect[""]="";
 		// $redirect[""]="";
 
 		// accommodaties
-		$redirect["http://test.venturasol.nl/skivakanties/accomodatie/les-trois-vallees/les-menuires-le-hameau-des-marmottes-8-kamer-chalet-7-slaapkamers-max-14-pers"]="http://test.venturasol.nl/wintersport/f8275/Chalet-Le-Hameau-des-Marmottes-8-kamer";
-		$redirect["http://test.venturasol.nl/skivakanties/accomodatie/les-trois-vallees-600km-piste-les-menuires-appartementen/chalet-appartementen-le-hameau-des-marmottes"]="http://test.venturasol.nl/wintersport/f8345/Chalet-appartement-Le-Hameau-des-Marmottes-3-kamer";
-		$redirect["http://test.venturasol.nl/skivakanties/accomodatie/les-trois-vallees/les-menuires-le-hameau-des-marmottes-6-kamer-chalet-5-slaapkamers-sauna-max-10-personen"]="http://test.venturasol.nl/wintersport/f8268/Chalet-Le-Hameau-des-Marmottes-6-kamer-met-sauna";
-		$redirect["http://test.venturasol.nl/skivakanties/accomodatie/les-trois-vallees/les-menuires-le-hameau-des-marmottes-7-kamer-chalet-6-slaapkamers-max-12-pers"]="http://test.venturasol.nl/wintersport/f8274/Chalet-Le-Hameau-des-Marmottes-7-kamer";
-		$redirect["http://test.venturasol.nl/skivakanties/accomodatie/les-trois-vallees/les-menuires-le-hameau-des-marmottes-8-kamer-chalet-7-slaapkamers-max-14-pers"]="http://test.venturasol.nl/wintersport/f8275/Chalet-Le-Hameau-des-Marmottes-8-kamer";
-		$redirect["http://test.venturasol.nl/skivakanties/accomodatie/les-trois-vallees-600km-piste-les-menuires-appartementen/les-alpages-de-reberty-copy"]="http://test.venturasol.nl/wintersport/f8365/Chalet-appartement-Les-Alpages-de-Reberty-2-kamer-cabine-zondag-t-m-zondag";
-		$redirect["http://test.venturasol.nl/skivakanties/accomodatie/les-trois-vallees-600km-piste-les-menuires-appartementen/les-alpages-de-reberty"]="http://test.venturasol.nl/wintersport/f8356/Chalet-appartement-Penthouse-Les-Alpages-de-Reberty-5-kamer-zondag-t-m-zondag";
+		$redirect["http://www.venturasol.nl/skivakanties/accomodatie/les-trois-vallees/les-menuires-le-hameau-des-marmottes-8-kamer-chalet-7-slaapkamers-max-14-pers"]="http://www.venturasol.nl/wintersport/f8275/Chalet-Le-Hameau-des-Marmottes-8-kamer";
+		$redirect["http://www.venturasol.nl/skivakanties/accomodatie/les-trois-vallees-600km-piste-les-menuires-appartementen/chalet-appartementen-le-hameau-des-marmottes"]="http://www.venturasol.nl/wintersport/f8345/Chalet-appartement-Le-Hameau-des-Marmottes-3-kamer";
+		$redirect["http://www.venturasol.nl/skivakanties/accomodatie/les-trois-vallees/les-menuires-le-hameau-des-marmottes-6-kamer-chalet-5-slaapkamers-sauna-max-10-personen"]="http://www.venturasol.nl/wintersport/f8268/Chalet-Le-Hameau-des-Marmottes-6-kamer-met-sauna";
+		$redirect["http://www.venturasol.nl/skivakanties/accomodatie/les-trois-vallees/les-menuires-le-hameau-des-marmottes-7-kamer-chalet-6-slaapkamers-max-12-pers"]="http://www.venturasol.nl/wintersport/f8274/Chalet-Le-Hameau-des-Marmottes-7-kamer";
+		$redirect["http://www.venturasol.nl/skivakanties/accomodatie/les-trois-vallees/les-menuires-le-hameau-des-marmottes-8-kamer-chalet-7-slaapkamers-max-14-pers"]="http://www.venturasol.nl/wintersport/f8275/Chalet-Le-Hameau-des-Marmottes-8-kamer";
+		$redirect["http://www.venturasol.nl/skivakanties/accomodatie/les-trois-vallees-600km-piste-les-menuires-appartementen/les-alpages-de-reberty-copy"]="http://www.venturasol.nl/wintersport/f8365/Chalet-appartement-Les-Alpages-de-Reberty-2-kamer-cabine-zondag-t-m-zondag";
+		$redirect["http://www.venturasol.nl/skivakanties/accomodatie/les-trois-vallees-600km-piste-les-menuires-appartementen/les-alpages-de-reberty"]="http://www.venturasol.nl/wintersport/f8356/Chalet-appartement-Penthouse-Les-Alpages-de-Reberty-5-kamer-zondag-t-m-zondag";
 
-		$current_url="http://test.venturasol.nl".$_SERVER["REQUEST_URI"];
+		$current_url="http://www.venturasol.nl".$_SERVER["REQUEST_URI"];
 		if($redirect[$current_url]) {
 			header("Location: ".$redirect[$current_url],true,301);
 			exit;
@@ -88,7 +90,7 @@ if(ereg("/pic/chalets/",$_SERVER["REQUEST_URI"])) {
 	}
 
 	# mensen doorsturen naar zoekformulier (bijv. bij http://www.chalet.nl/zwitserland)
-	if(eregi("^/([0-9a-zA-Z]+)$",$_SERVER["REQUEST_URI"],$regs404)) {
+	if(eregi("^/([0-9a-zA-Z]+)$",$_SERVER["REQUEST_URI"],$regs404) and $_SERVER["HTTP_HOST"]!="www.venturasol.nl") {
 		include("admin/vars.php");
 		$_SERVER["REDIRECT_STATUS"]="404";
 		wt_404();
