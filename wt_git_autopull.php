73b332b68ce24e0ed5c39eecd1d20204d03bd5f1
Jeroen Boschman <jeroen@webtastic.nl>
2013-10-09 17:45:35 +0200

    test3

--- wt_git_autopull.php
+++ wt_git_autopull.php
@@ -2,22 +2,30 @@
 
 include("/var/www/chalet.nl/html_test/admin/allfunctions.php");
 
+$checkfile = "/var/www/chalet.nl/html_test/tmp/git-autopull-acceptance-test.txt";
+
 if($argv[1]=="cron") {
+
+	if($argv[2]=="sleep") {
+		sleep(30);
+	}
+
+	if(file_exists($checkfile)) {
+		unlink($checkfile);
+		system("cd /var/www/chalet.nl/html_test/;git fetch origin;git reset --hard origin/acceptance-test");
+	}
 
 } elseif($_POST["payload"]) {
 
 	$obj = json_decode($_POST["payload"], true);
 
+	if(preg_match("@acceptance-test@",$obj["ref"])) {
+		touch($checkfile);
+	}
+
 	wt_mail("jeroen@webtastic.nl","POST guthub",wt_dump($obj,false));
 }
 
-
-
-
-
-
-
-
-
+echo "OK";
 
 ?>
