28d4f3b5118e26083c2eec7cf1f8104586d0cad8
Jeroen Boschman <jeroen@webtastic.nl>
2013-10-09 10:51:04 +0200

    hopefully the last https-changes for the acceptance-server

--- nieuwsbrief.php
+++ nieuwsbrief.php
@@ -7,7 +7,7 @@ if(!$vars["nieuwsbrief_aanbieden"]) {
 	exit;
 } else {
 
-	if($vars["website"]=="C" and $_SERVER["HTTPS"]<>"on" and !$vars["lokale_testserver"]) {
+	if($vars["website"]=="C" and $_SERVER["HTTPS"]<>"on" and !$vars["lokale_testserver"] and !$vars["acceptatie_testserver"]) {
 		# deze pagina altijd via https
 		header("Location: https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
 		exit;
