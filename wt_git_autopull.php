45fd21040416efe02f984816e3854dbfb24b329f
Jeroen Boschman <jeroen@webtastic.nl>
2013-10-09 18:26:26 +0200

    changes regarding Git

--- wt_git_autopull.php
+++ wt_git_autopull.php
@@ -1,23 +1,38 @@
 <?php
 
+//
+// script to autopull Git commits
+//
+
 include("/var/www/chalet.nl/html_test/admin/allfunctions.php");
+
+$checkfile = "/var/www/chalet.nl/html_test/tmp/git-autopull-acceptance-test.txt";
 
 if($argv[1]=="cron") {
 
-} elseif($_POST["payload"]) {
+	if($argv[2]=="sleep") {
+		sleep(30);
+	}
 
-	$obj = json_decode($_POST["payload"], true);
+	if(file_exists($checkfile)) {
+		unlink($checkfile);
+		passthru("cd /var/www/chalet.nl/html_test/;git fetch origin;git reset --hard origin/acceptance-test");
+	}
 
-	wt_mail("jeroen@webtastic.nl","POST guthub",wt_dump($obj,false));
+} else {
+
+	if($_POST["payload"]) {
+
+		$obj = json_decode($_POST["payload"], true);
+
+		if(preg_match("@acceptance-test@",$obj["ref"])) {
+			touch($checkfile);
+			wt_mail("jeroen@webtastic.nl","POST guthub",wt_dump($obj,false));
+		}
+
+	}
+	echo "OK";
 }
-
-
-
-
-
-
-
-
 
 
 ?>
