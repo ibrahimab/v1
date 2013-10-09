f3cb5c16d3c1f8f5c6341d4a3620a49a67d8ccbc
Jeroen Boschman <jeroen@webtastic.nl>
2013-10-09 16:15:49 +0200

    test2

--- wt_git_autopull.php
+++ wt_git_autopull.php
@@ -4,8 +4,11 @@ include("/var/www/chalet.nl/html_test/admin/allfunctions.php");
 
 if($argv[1]=="cron") {
 
-} elseif($_POST) {
-	wt_mail("jeroen@webtastic.nl","POST guthub",wt_dump($_POST,false));
+} elseif($_POST["payload"]) {
+
+	$obj = json_decode($_POST["payload"], true);
+
+	wt_mail("jeroen@webtastic.nl","POST guthub",wt_dump($obj,false));
 }
 
 
