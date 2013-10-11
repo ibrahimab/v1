f5616c273e52f939321caaa6c2e902ff55daae3b
NetRom <chalet@netrom.ro>
2013-10-11 11:40:21 +0300

    Added restriction to show payment options only for chalet.nl, chalet.be and chalet.eu domains

--- bsys_payments.php
+++ bsys_payments.php
@@ -9,6 +9,11 @@ include("admin/vars.php");
 
 $breadcrumbs["last"] = txt("payment", "bsys");
 
+if($vars["website"] != "C" && $vars["website"] != "E" && $vars["website"] != "B") {
+	header("Location: ".$path);
+	exit;
+}
+
 #
 # Indien _GET leeg is (en dus alleen boeken.php is opgevraagd): terug naar hoofdpagina
 #
