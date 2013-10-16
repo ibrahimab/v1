2b4227de8f86e7cfb3ea496c67ddbe649117e6d5
Jeroen Boschman <jeroen@webtastic.nl>
2013-10-16 15:49:52 +0200

    easy copy of live to test database

---
+++ admin/test_database_query.sql
@@ -0,0 +1,4 @@
+
+-- create NetRom-user for CMS
+INSERT INTO `user` SET `user`='netrom', voornaam='NetRom', achternaam='Test', email='chalet@netrom.ro', password='_9db77c04e1bc6630c2bbc99d787aab07030b4b08', userlevel=1, priv='22,24,17,14,16,11,12,15,13,7,29,3,9,1,5,25,21,6,23,27,28,30,2,10,26,8';
+
