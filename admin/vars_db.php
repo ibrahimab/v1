79a578ccb165b8a37a61efe346bb3fb04e40d792
NetRom <chalet@netrom.ro>
2013-10-18 11:39:18 +0300

    Testing DocData database connection #3

--- admin/vars_db.php
+++ admin/vars_db.php
@@ -4,6 +4,23 @@
 // database-credentials
 //
 
+// is this the acceptation-testserver?
+if(preg_match("@^test\.@",$_SERVER["HTTP_HOST"]) or preg_match("@/html_test/@",$_SERVER["SCRIPT_FILENAME"])) {
+	$vars["acceptatie_testserver"]=true;
+
+	if($_SERVER["REMOTE_ADDR"]=="31.223.173.113" or $_SERVER["REMOTE_ADDR"]=="37.34.56.191") {
+		// IP WebTastic
+		$vars["lokale_testserver_mailadres"]="testform_ss@webtastic.nl";
+	} elseif($_SERVER["REMOTE_ADDR"]=="213.125.152.154") {
+		// IP Chalet
+		$vars["lokale_testserver_mailadres"]="bjorn@chalet.nl";
+	} elseif($_SERVER["REMOTE_ADDR"]=="82.77.165.60" or $_SERVER["REMOTE_ADDR"]=="194.102.98.240") {
+		$vars["lokale_testserver_mailadres"]="chalet@netrom.ro";
+	} else {
+		$vars["lokale_testserver_mailadres"]="testform_ss@webtastic.nl";
+	}
+}
+
 if(netrom_testserver) {
 	$mysqlsettings["name"]["remote"]="dbtest_chalet";	# Databasenaam bij provider
 	$mysqlsettings["user"]="chalet-nl-usr";		# Username bij provider
