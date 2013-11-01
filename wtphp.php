fe5f8519b97f020c1858ed65ad3b64bcecc89081
Jeroen Boschman <jeroen@webtastic.nl>
2013-11-01 14:27:38 +0100

    created errortest

--- wtphp.php
+++ wtphp.php
@@ -1,5 +1,16 @@
 <?php
 
+include("admin/vars.php");
+
+if($_GET["errortest"]) {
+	echo "Errortest has been sent...";
+	sort($empty_test_array);
+} else {
+	if($_COOKIE["flc"]) {
+		phpinfo();
+	}
+}
+
 # test 10
 # test
 # TEST 8-10-2013
@@ -13,8 +24,12 @@
 
 #error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 
-#if($_COOKIE["flc"]) {
-	phpinfo();
-#}
+
+
+//http://chalet-dev.web.netromtest.ro/error_log.php?error=
+
+// $a=file_get_contents("http://www.webtastic.nl/ipadres/direct.php");
+// echo "IPADRES:".$a;
+
 
 ?>
