810462ed7a7c9ae0cd9c55ee60a3c83430def129
NetRom <chalet@netrom.ro>
2013-11-21 15:52:21 +0200

    Updated Docdata module in accordance with the remarks from CHA-61 item.

--- content/_teksten.php
+++ content/_teksten.php
@@ -4119,6 +4119,9 @@ $txt["en"]["bsys"]["totaalbedrag"]="Full payment";
 $txt["nl"]["bsys"]["advance_payment"]="Aanbetaling [[v_no]]betalen";
 $txt["en"]["bsys"]["advance_payment"]="Advance payment";
 
+$txt["nl"]["bsys"]["complete_advance_payment"]="Pay complete down-payment";
+$txt["en"]["bsys"]["complete_advance_payment"]="Pay complete down-payment";
+
 $txt["nl"]["bsys"]["full_payment"]="Het totale bedrag betalen";
 $txt["en"]["bsys"]["full_payment"]="Full payment";
 
