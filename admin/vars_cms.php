a613463e2b989868bcc0a422814e8ad4d4a00947
Jeroen Boschman <jeroen@webtastic.nl>
2014-03-21 15:19:49 +0100

    disable order by fax

--- admin/vars_cms.php
+++ admin/vars_cms.php
@@ -1016,7 +1016,7 @@ $vars["bijkomendekosten_perboekingpersoon"]=array(1=>"per boeking",2=>"per perso
 $vars["bijkomendekosten_gekoppeldaan"]=array(1=>"accommodaties/types",2=>"skipassen",3=>"opties");
 $vars["status_bestaandeklanten"]=array(0=>"mail sturen",1=>"mail verstuurd",2=>"nabellen",3=>"opnieuw mailen",4=>"optie bij ons aangevraagd",5=>"geboekt bij ons",6=>"elders geboekt",7=>"geen belangstelling");
 $vars["themakleurencombinatie"]=array(1=>"kleurencombinatie 1",2=>"kleurencombinatie 2",3=>"kleurencombinatie 3",4=>"kleurencombinatie 4",5=>"kleurencombinatie 5",6=>"kleurencombinatie 6",7=>"kleurencombinatie 7",8=>"kleurencombinatie 8",9=>"kleurencombinatie 9");
-$vars["standaardbestelmanier"]=array(1=>"via e-mail", 2=>"via fax", 3=>"via XML", 4=>"via inlog op website", 5=>"interne verwerking");
+$vars["standaardbestelmanier"]=array(1=>"via e-mail", 3=>"via XML", 4=>"via inlog op website", 5=>"interne verwerking");
 $vars["bevestigmethode"]=array(1=>"stuurt direct een factuur",2=>"bevestigt zonder reserveringsnummer",3=>"bevestigt met reserveringsnummer");
 #$vars["nummers_voorraad_velden"]=array("voorraad_garantie","voorraad_allotment","voorraad_vervallen_allotment","voorraad_optie_leverancier","voorraad_xml","voorraad_request","voorraad_optie_klant","voorraad_bijwerken");
 # zonder XML:
--- cms_bestelmail.php
+++ cms_bestelmail.php
@@ -57,13 +57,7 @@ if($db->next_record()) {
 $form=new form2("frm");
 $form->settings["fullname"]="Bestelmailfax";
 $form->settings["layout"]["css"]=false;
-if($_GET["t"]==1) {
-	$form->settings["message"]["submitbutton"]["nl"]="BESTELLING MAILEN";
-} else {
-	$form->settings["message"]["submitbutton"]["nl"]="FAX PRINTEN";
-	$form->settings["goto"]=$_GET["burl"];
-	$form->settings["target"]="_blank";
-}
+$form->settings["message"]["submitbutton"]["nl"]="BESTELLING MAILEN";
 
 #_field: (obl),id,title,db,prevalue,options,layout
 if($gegevens["stap1"]["verzameltype"]) {
@@ -74,9 +68,7 @@ if($gegevens["stap1"]["verzameltype"]) {
 	}
 }
 
-if($_GET["t"]==1) {
-	$form->field_htmlcol("aan","Mailadres",array("text"=>$vars["temp_leverancier"]["email"]));
-}
+$form->field_htmlcol("aan","Mailadres",array("text"=>$vars["temp_leverancier"]["email"]));
 $form->field_text(1,"contactpersoon",$vars["bestelmailfax_beste"][$bmftaal],"",array("text"=>$vars["temp_leverancier"]["contactpersoon"])); # (opslaan in databaseveld "test")
 
 // eerder afgeboekte voorraad bepalen
@@ -230,246 +222,98 @@ if($gegevens["stap1"]["flexibel"] or $gegevens["stap1"]["verblijfsduur"]>1) {
 $form->field_textarea(0,"extra",$vars["bestelmailfax_extra"][$bmftaal],"",array("text"=>$gegevens["stap1"]["opmerkingen_voucher"]));
 #$form->field_text(1,"ondertekennaam",$vars["bestelmailfax_metvriendelijkegroet"][$bmftaal],"",array("text"=>wt_naam($login->vars["voornaam"],$login->vars["tussenvoegsel"],$login->vars["achternaam"])));
 $form->field_text(1,"ondertekennaam",$vars["bestelmailfax_metvriendelijkegroet"][$bmftaal],"",array("text"=>$login->vars["voornaam"]));
-if($_GET["t"]==1) {
-	$form->field_select(1,"afzendmailadres","Versturen vanaf","",array("selection"=>($_GET["info"] ? 2 : 1)),array("selection"=>array(1=>$login->vars["email"],2=>"info@chalet.nl")));
-}
+$form->field_select(1,"afzendmailadres","Versturen vanaf","",array("selection"=>($_GET["info"] ? 2 : 1)),array("selection"=>array(1=>$login->vars["email"],2=>"info@chalet.nl")));
+
 
 $form->check_input();
 
 if($form->okay) {
-	if($_GET["t"]==1) {
 
-		# Klantnaam opslaan
-		$db->query("UPDATE boeking SET aan_leverancier_doorgegeven_naam='".addslashes($form->input["clientsname"])."' WHERE boeking_id='".intval($_GET["bid"])."';");
+	# Klantnaam opslaan
+	$db->query("UPDATE boeking SET aan_leverancier_doorgegeven_naam='".addslashes($form->input["clientsname"])."' WHERE boeking_id='".intval($_GET["bid"])."';");
 
-		unset($html);
-		$html.="<html><body style=\"font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 0.8em;\">";
-		$html.="<p>".nl2br(htmlentities($vars["temp_leverancier"]["adresregels"]))."<p>";
-		$html.=htmlentities($vars["bestelmailfax_beste"][$bmftaal])." ".htmlentities($form->input["contactpersoon"]).",<p>";
-		$html.=htmlentities($vars["bestelmailfax_hierbijwillenwe"][$bmftaal]);
-		if($_GET["reserveringsnummer"]) {
-			$html.=": ".wt_he($vars["bestelmailfax_soort"][$bmftaal][4]." ".$_GET["reserveringsnummer"]);
-		} else {
-			$html.=".";
-		}
-		$html.="<p>";
-		$html.=htmlentities($vars["bestelmailfax_dezeacchebbenwij"][$bmftaal])." ".htmlentities($vars["bestelmailfax_soort"][$bmftaal][$form->input["soort"]]);
-		if($form->input["geldig"]) $html.=" ".htmlentities($vars["bestelmailfax_tot"][$bmftaal])." ".htmlentities($form->input["geldig"]);
-		$html.=":<p>";
-		$html.="<table cellspacing=\"0\" cellpadding=\"0\" style=\"font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 1.0em;\">";
-		$html.="<tr><td>".htmlentities($vars["bestelmailfax_klantnaam"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["clientsname"])."</td></tr>";
-		$html.="<tr><td colspan=\"3\">&nbsp;</td></tr>";
-		$html.="<tr><td>".htmlentities($vars["bestelmailfax_aankomst"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["dateofarrival"])."</td></tr>";
-		$html.="<tr><td>".htmlentities($vars["bestelmailfax_verblijfsduur"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["stayingtime"])."</td></tr>";
-		$html.="<tr><td colspan=\"3\">&nbsp;</td></tr>";
-		$html.="<tr><td>".htmlentities($vars["bestelmailfax_plaats"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["resort"])."</td></tr>";
-		$html.="<tr><td>".htmlentities($vars["bestelmailfax_accommodatie"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["accommodation"])."</td></tr>";
-		$html.="<tr><td>".htmlentities($vars["bestelmailfax_type"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["type"])."</td></tr>";
-		$html.="<tr><td>".htmlentities($vars["bestelmailfax_maxcapaciteit"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["maxcapacity"])."</td></tr>";
-		$html.="<tr><td colspan=\"3\">&nbsp;</td></tr>";
-		$html.="<tr><td>".htmlentities($vars["bestelmailfax_prijs"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["price"])."</td></tr>";
-		$html.="<tr><td>".htmlentities($vars["bestelmailfax_korting"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["commission"])."</td></tr>";
-		if($form->input["vroegboekkorting"]) $html.="<tr><td>".htmlentities($vars["bestelmailfax_vroegboekkorting"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["vroegboekkorting"])."</td></tr>";
-
-		if($form->input["extra"]) {
-			$html.="<tr><td colspan=\"3\">&nbsp;</td></tr>";
-			$html.="<tr><td valign=\"top\">".htmlentities($vars["bestelmailfax_extra"][$bmftaal])."</td><td valign=\"top\">&nbsp;&nbsp;&nbsp;:&nbsp;</td><td valign=\"top\">".nl2br(htmlentities($form->input["extra"]))."</td></tr>";
-		}
-		$html.="</table><p>";
-		$html.=htmlentities($vars["bestelmailfax_graagpermailoffaxbevestigen"][$bmftaal])."<p>";
-		$html.=htmlentities($vars["bestelmailfax_metvriendelijkegroet"][$bmftaal]).",<br><br>";
-		$html.=htmlentities($form->input["ondertekennaam"])."<p>";
-		if($gegevens["stap1"]["accinfo"]["wzt"]==2) {
-			$temp_websitenaam="Chalet.nl / Zomerhuisje.nl";
-		} else {
-			$temp_websitenaam="Chalet.nl";
-		}
-		$html.=$temp_websitenaam."<br>Wipmolenlaan 3<br>3447 GJ Woerden<br>KvK: 30209634<br>Tel: +31 (0)348 - 43 46 49<br>Fax: +31 (0)348 - 69 07 52<br>E-mail: <a href=\"mailto:info@chalet.nl\">info@chalet.nl</a><p>";
-		$html.="</body></html>";
-		$mail=new wt_mail;
-		$mail->fromname=$temp_websitenaam;
-		if($form->input["afzendmailadres"]==1) {
-			$mail->from=$login->vars["email"];
-		} else {
-			$mail->from="info@chalet.nl";
-		}
-		$mail->subject=$temp_websitenaam." ".$vars["bestelmailfax_klant"][$bmftaal]." ".$form->input["clientsname"]." ".$form->input["dateofarrival"]." ".$form->input["resort"]." / ".$form->input["accommodation"].($form->input["type"] ? " ".$form->input["type"] : "");
-		$mail->html=$html;
-
-		# mailtje naar leverancier sturen
-		$mail->to=$vars["temp_leverancier"]["email"];
-		$mail->send();
-
-		# mailtje ook naar chalet sturen
-		if($form->input["afzendmailadres"]==1) {
-			$mail->to=$login->vars["email"];
-		} else {
-			$mail->to="info@chalet.nl";
-		}
-		$mail->send();
-
-		if($_GET["oaid"]) {
-			# Status optieaanvraag veranderen in "geboekt"
-			$db->query("UPDATE optieaanvraag SET status=8 WHERE optieaanvraag_id='".addslashes($_GET["oaid"])."';");
-		} else {
-
-			# bestelstatus op 'bevestiging afwachten' zetten en besteldatum op vandaag zetten
-			$db->query("UPDATE boeking SET bestelstatus=2, besteldatum=NOW() WHERE boeking_id='".addslashes($gegevens["stap1"]["boekingid"])."';");
-
-			# Loggen bij boeking
-			chalet_log("bestelmail ".$vars["bestelmailfax_soort"]["N"][$form->input["soort"]]." verzonden naar ".$vars["temp_leverancier"]["email"],false,true);
-			chalet_log("besteldatum op ".date("d-m-Y")." gezet, bestelstatus veranderd in 'bevestiging afwachten'",false,true);
-		}
+	unset($html);
+	$html.="<html><body style=\"font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 0.8em;\">";
+	$html.="<p>".nl2br(htmlentities($vars["temp_leverancier"]["adresregels"]))."<p>";
+	$html.=htmlentities($vars["bestelmailfax_beste"][$bmftaal])." ".htmlentities($form->input["contactpersoon"]).",<p>";
+	$html.=htmlentities($vars["bestelmailfax_hierbijwillenwe"][$bmftaal]);
+	if($_GET["reserveringsnummer"]) {
+		$html.=": ".wt_he($vars["bestelmailfax_soort"][$bmftaal][4]." ".$_GET["reserveringsnummer"]);
 	} else {
+		$html.=".";
+	}
+	$html.="<p>";
+	$html.=htmlentities($vars["bestelmailfax_dezeacchebbenwij"][$bmftaal])." ".htmlentities($vars["bestelmailfax_soort"][$bmftaal][$form->input["soort"]]);
+	if($form->input["geldig"]) $html.=" ".htmlentities($vars["bestelmailfax_tot"][$bmftaal])." ".htmlentities($form->input["geldig"]);
+	$html.=":<p>";
+	$html.="<table cellspacing=\"0\" cellpadding=\"0\" style=\"font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 1.0em;\">";
+	$html.="<tr><td>".htmlentities($vars["bestelmailfax_klantnaam"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["clientsname"])."</td></tr>";
+	$html.="<tr><td colspan=\"3\">&nbsp;</td></tr>";
+	$html.="<tr><td>".htmlentities($vars["bestelmailfax_aankomst"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["dateofarrival"])."</td></tr>";
+	$html.="<tr><td>".htmlentities($vars["bestelmailfax_verblijfsduur"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["stayingtime"])."</td></tr>";
+	$html.="<tr><td colspan=\"3\">&nbsp;</td></tr>";
+	$html.="<tr><td>".htmlentities($vars["bestelmailfax_plaats"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["resort"])."</td></tr>";
+	$html.="<tr><td>".htmlentities($vars["bestelmailfax_accommodatie"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["accommodation"])."</td></tr>";
+	$html.="<tr><td>".htmlentities($vars["bestelmailfax_type"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["type"])."</td></tr>";
+	$html.="<tr><td>".htmlentities($vars["bestelmailfax_maxcapaciteit"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["maxcapacity"])."</td></tr>";
+	$html.="<tr><td colspan=\"3\">&nbsp;</td></tr>";
+	$html.="<tr><td>".htmlentities($vars["bestelmailfax_prijs"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["price"])."</td></tr>";
+	$html.="<tr><td>".htmlentities($vars["bestelmailfax_korting"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["commission"])."</td></tr>";
+	if($form->input["vroegboekkorting"]) $html.="<tr><td>".htmlentities($vars["bestelmailfax_vroegboekkorting"][$bmftaal])."</td><td>&nbsp;&nbsp;&nbsp;:&nbsp;</td><td>".htmlentities($form->input["vroegboekkorting"])."</td></tr>";
 
-		if($gegevens["stap1"]["accinfo"]["wzt"]==2) {
-			$vars["temp_websitenaam"]="Chalet.nl / Zomerhuisje.nl";
-		} else {
-			$vars["temp_websitenaam"]="Chalet.nl";
-		}
+	if($form->input["extra"]) {
+		$html.="<tr><td colspan=\"3\">&nbsp;</td></tr>";
+		$html.="<tr><td valign=\"top\">".htmlentities($vars["bestelmailfax_extra"][$bmftaal])."</td><td valign=\"top\">&nbsp;&nbsp;&nbsp;:&nbsp;</td><td valign=\"top\">".nl2br(htmlentities($form->input["extra"]))."</td></tr>";
+	}
+	$html.="</table><p>";
+	$html.=htmlentities($vars["bestelmailfax_graagpermailoffaxbevestigen"][$bmftaal])."<p>";
+	$html.=htmlentities($vars["bestelmailfax_metvriendelijkegroet"][$bmftaal]).",<br><br>";
+	$html.=htmlentities($form->input["ondertekennaam"])."<p>";
+	if($gegevens["stap1"]["accinfo"]["wzt"]==2) {
+		$temp_websitenaam="Chalet.nl / Zomerhuisje.nl";
+	} else {
+		$temp_websitenaam="Chalet.nl";
+	}
+	$html.=$temp_websitenaam."<br>Wipmolenlaan 3<br>3447 GJ Woerden<br>KvK: 30209634<br>Tel: +31 (0)348 - 43 46 49<br>Fax: +31 (0)348 - 69 07 52<br>E-mail: <a href=\"mailto:info@chalet.nl\">info@chalet.nl</a><p>";
+	$html.="</body></html>";
+	$mail=new wt_mail;
+	$mail->fromname=$temp_websitenaam;
+	if($form->input["afzendmailadres"]==1) {
+		$mail->from=$login->vars["email"];
+	} else {
+		$mail->from="info@chalet.nl";
+	}
+	$mail->subject=$temp_websitenaam." ".$vars["bestelmailfax_klant"][$bmftaal]." ".$form->input["clientsname"]." ".$form->input["dateofarrival"]." ".$form->input["resort"]." / ".$form->input["accommodation"].($form->input["type"] ? " ".$form->input["type"] : "");
+	$mail->html=$html;
+
+	# mailtje naar leverancier sturen
+	$mail->to=$vars["temp_leverancier"]["email"];
+	$mail->send();
+
+	# mailtje ook naar chalet sturen
+	if($form->input["afzendmailadres"]==1) {
+		$mail->to=$login->vars["email"];
+	} else {
+		$mail->to="info@chalet.nl";
+	}
+	$mail->send();
+
+	if($_GET["oaid"]) {
+		# Status optieaanvraag veranderen in "geboekt"
+		$db->query("UPDATE optieaanvraag SET status=8 WHERE optieaanvraag_id='".addslashes($_GET["oaid"])."';");
+	} else {
 
 		# bestelstatus op 'bevestiging afwachten' zetten en besteldatum op vandaag zetten
 		$db->query("UPDATE boeking SET bestelstatus=2, besteldatum=NOW() WHERE boeking_id='".addslashes($gegevens["stap1"]["boekingid"])."';");
 
 		# Loggen bij boeking
-		chalet_log("bestelfax ".$vars["bestelmailfax_soort"]["N"][$form->input["soort"]]." aangemaakt",false,true);
+		chalet_log("bestelmail ".$vars["bestelmailfax_soort"]["N"][$form->input["soort"]]." verzonden naar ".$vars["temp_leverancier"]["email"],false,true);
 		chalet_log("besteldatum op ".date("d-m-Y")." gezet, bestelstatus veranderd in 'bevestiging afwachten'",false,true);
-
-		require("admin/fpdf.php");
-
-		class PDF extends FPDF {
-
-			function _getfontpath() {
-				return "pdf/fonts/";
-			}
-
-			function Header() {
-				if($this->gegevens["stap1"]["website_specifiek"]["websitetype"]==3) {
-					$this->Image('pic/factuur_logo_vakantiewoningen.png',10,8,50);
-				} elseif($this->gegevens["stap1"]["website_specifiek"]["websitetype"]==2) {
-					if($this->vars["temp_leverancier"]["bestelfax_logo"]) {
-						$this->Image('pic/factuur_logo_wsa.png',10,10,100);
-					} else {
-						$this->Image('pic/factuur_logo.png',10,8,50);
-					}
-				} else {
-					$this->Image('pic/factuur_logo.png',10,8,50);
-				}
-				$this->SetFont('Arial','',10);
-				$this->MultiCell(0,4,"".$this->vars["temp_websitenaam"]."\nWipmolenlaan 3\n3447 GJ Woerden\n\nTel.: +31 348 434649\nFax: +31 348 690752\nKvK nr. 30209634\n\nBankrek. 84.93.06.671\nBTW NL-8169.23.462.B.01\n\nIBAN: NL21 ABNA 0849 3066 71\nBIC: ABNANL2A\nABN AMRO - Woerden",0,"R");
-				$this->Ln(20);
-			}
-
-			function Footer() {
-				$this->SetY(-20);
-				$this->SetFont('Arial','I',8);
-				$this->Cell(0,10,$this->PageNo().'/{nb}',0,0,'C');
-			}
-		}
-
-		$pdf=new PDF();
-		$pdf->gegevens=$gegevens;
-		$pdf->vars=$vars;
-
-		$pdf->AliasNbPages();
-		$pdf->AddPage();
-		$pdf->SetY(60);
-		$pdf->MultiCell(150,4,$vars["temp_leverancier"]["adresregels"],0,'L',0);
-		$pdf->Ln();
-		$pdf->Ln();
-		$pdf->Cell(0,4,"Woerden, ".DATUM("D")." ".date("F Y"),0,1);
-		$pdf->Ln();
-		$pdf->Cell(0,4,"fax: ".$vars["temp_leverancier"]["faxnummer"],0,1);
-		$pdf->Ln();
-		$pdf->Ln();
-		$pdf->Cell(0,4,$vars["bestelmailfax_beste"][$bmftaal]." ".$form->input["contactpersoon"].",",0,1);
-		$pdf->Ln();
-		$pdf->Cell(0,4,$vars["bestelmailfax_hierbijwillenwe"][$bmftaal].".",0,1);
-		$pdf->Ln();
-		$pdf->Cell(0,4,$vars["bestelmailfax_dezeacchebbenwij"][$bmftaal]." ".$vars["bestelmailfax_soort"][$bmftaal][$form->input["soort"]].($form->input["geldig"] ? " ".$vars["bestelmailfax_tot"][$bmftaal]." ".$form->input["geldig"] : "").":",0,1);
-		$pdf->Ln();
-
-		$pdf->Cell(35,4,$vars["bestelmailfax_klantnaam"][$bmftaal],0,0,'L',0);
-		$pdf->Cell(5,4,":",0,0,'L',0);
-		$pdf->Cell(50,4,$form->input["clientsname"],0,0,'L',0);
-		$pdf->Ln();
-		$pdf->Ln();
-
-		$pdf->Cell(35,4,$vars["bestelmailfax_aankomst"][$bmftaal],0,0,'L',0);
-		$pdf->Cell(5,4,":",0,0,'L',0);
-		$pdf->Cell(50,4,$form->input["dateofarrival"],0,0,'L',0);
-		$pdf->Ln();
-
-		$pdf->Cell(35,4,$vars["bestelmailfax_verblijfsduur"][$bmftaal],0,0,'L',0);
-		$pdf->Cell(5,4,":",0,0,'L',0);
-		$pdf->Cell(50,4,$form->input["stayingtime"],0,0,'L',0);
-		$pdf->Ln();
-		$pdf->Ln();
-
-		$pdf->Cell(35,4,$vars["bestelmailfax_plaats"][$bmftaal],0,0,'L',0);
-		$pdf->Cell(5,4,":",0,0,'L',0);
-		$pdf->Cell(50,4,$form->input["resort"],0,0,'L',0);
-		$pdf->Ln();
-
-		$pdf->Cell(35,4,$vars["bestelmailfax_accommodatie"][$bmftaal],0,0,'L',0);
-		$pdf->Cell(5,4,":",0,0,'L',0);
-		$pdf->Cell(50,4,$form->input["accommodation"],0,0,'L',0);
-		$pdf->Ln();
-
-		$pdf->Cell(35,4,$vars["bestelmailfax_type"][$bmftaal],0,0,'L',0);
-		$pdf->Cell(5,4,":",0,0,'L',0);
-		$pdf->Cell(50,4,$form->input["type"],0,0,'L',0);
-		$pdf->Ln();
-
-		$pdf->Cell(35,4,$vars["bestelmailfax_maxcapaciteit"][$bmftaal],0,0,'L',0);
-		$pdf->Cell(5,4,":",0,0,'L',0);
-		$pdf->Cell(50,4,$form->input["maxcapacity"],0,0,'L',0);
-		$pdf->Ln();
-		$pdf->Ln();
-
-		$pdf->Cell(35,4,$vars["bestelmailfax_prijs"][$bmftaal],0,0,'L',0);
-		$pdf->Cell(5,4,":",0,0,'L',0);
-		$pdf->Cell(50,4,$form->input["price"],0,0,'L',0);
-		$pdf->Ln();
-
-		$pdf->Cell(35,4,$vars["bestelmailfax_korting"][$bmftaal],0,0,'L',0);
-		$pdf->Cell(5,4,":",0,0,'L',0);
-		$pdf->Cell(50,4,$form->input["commission"],0,0,'L',0);
-		$pdf->Ln();
-
-		if($form->input["vroegboekkorting"]) {
-			$pdf->Cell(35,4,$vars["bestelmailfax_vroegboekkorting"][$bmftaal],0,0,'L',0);
-			$pdf->Cell(5,4,":",0,0,'L',0);
-			$pdf->Cell(50,4,$form->input["vroegboekkorting"],0,0,'L',0);
-			$pdf->Ln();
-		}
-
-		$pdf->Ln();
-
-		if($form->input["extra"]) {
-			$pdf->Cell(35,4,$vars["bestelmailfax_extra"][$bmftaal],0,0,'L',0);
-			$pdf->Cell(5,4,":",0,0,'L',0);
-			$pdf->MultiCell(150,4,$form->input["extra"],0,'L',0);
-			$pdf->Ln();
-			$pdf->Ln();
-		}
-
-		$pdf->Cell(0,4,$vars["bestelmailfax_graagpermailoffaxbevestigen"][$bmftaal],0,1);
-		$pdf->Ln();
-		$pdf->Ln();
-
-		$pdf->Cell(0,4,$vars["bestelmailfax_metvriendelijkegroet"][$bmftaal].",",0,1);
-		$pdf->Ln();
-		$pdf->Ln();
-		$pdf->Cell(0,4,$form->input["ondertekennaam"],0,1);
-
-		$pdf->Output();
-		exit;
 	}
 }
 
 $form->end_declaration();
 
-$layout->display_all("Bestellen via e-mail/fax");
+$layout->display_all("Bestellen via e-mail");
 
 ?>
