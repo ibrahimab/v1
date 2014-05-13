6f4c8b0e6a17e96eac4006c0614dd05fcc8f7e03
Jeroen Boschman <jeroen@webtastic.nl>
2014-05-13 14:06:03 +0200

    bugfix ChaletsinVallandry: search with type-number

--- zoek-en-boek.php
+++ zoek-en-boek.php
@@ -56,7 +56,14 @@ $vars["jquery_chosen"]=true;
 
 # Zoeken op accommodatiecode: redirect naar die accommodatie
 if(ereg("^[A-Za-z]([0-9]+)$",trim($_GET["fzt"]),$regs) and !$_GET["saved"]) {
-	$db->query("SELECT type_id, begincode FROM view_accommodatie WHERE type_id='".addslashes($regs[1])."' AND websites LIKE '%".$vars["website"]."%' AND atonen=1 AND ttonen=1 AND archief=0 AND wzt='".$vars["seizoentype"]."'");
+	if($vars["websitetype"]==6) {
+		// ChaletsinVallandry: seizoentype 1 and 2
+		$temp_seizoentype = "1,2";
+	} else {
+		// other sites
+		$temp_seizoentype = $vars["seizoentype"];
+	}
+	$db->query("SELECT type_id, begincode FROM view_accommodatie WHERE type_id='".addslashes($regs[1])."' AND websites LIKE '%".$vars["website"]."%' AND atonen=1 AND ttonen=1 AND archief=0 AND wzt IN (".$temp_seizoentype.");");
 	if($db->next_record()) {
 		header("Location: ".$path."accommodatie/".$db->f("begincode").$db->f("type_id")."/");
 		exit;
