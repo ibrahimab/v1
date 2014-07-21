382330a51c6eb0ae66a84cc8b6475b40a85176db
Jeroen Boschman <jeroen@webtastic.nl>
2014-07-21 10:50:06 +0200

    disabled "persberichten"

--- persberichten.php
+++ persberichten.php
@@ -1,7 +1,14 @@
 <?php
 
+
 $language_content=true;
 include("admin/vars.php");
+
+// pagina staat tijdelijk uit (21 juli 2014)
+header("Location: ".$vars["path"]."wie-zijn-wij.php");
+exit;
+
+
 include "content/opmaak.php";
 
 ?>
