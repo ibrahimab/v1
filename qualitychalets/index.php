c332c5bbcc8d3e9c27e4632d776ac7e88f23b4be
Jeroen Boschman <jeroen@webtastic.nl>
2014-08-12 18:20:06 +0200

    redirects qualitychalets.com

---
+++ qualitychalets/index.php
@@ -0,0 +1,140 @@
+<?php
+
+
+//
+// 301-redirects for qualitychalets.com
+//
+
+$redirect = array(
+"/nl/"=>"https://www.chalet.nl/",
+"/nl/chaletpark/"=>"https://www.chalet.nl/zoek-en-boek.php?scrolly=0&filled=1&fsg=&fsg_invoer=&fad=0&fap=0&fas=0&fzt=Hameau+des+Marmottes",
+"/nl/residences/"=>"https://www.chalet.nl/land/Frankrijk/",
+"/nl/residences/bourg-saint-maurice/"=>"https://www.chalet.nl/wintersport/plaats/Bourg-Saint-Maurice/",
+"/nl/residences/champagny/"=>"https://www.chalet.nl/wintersport/plaats/Champagny-en-Vanoise/",
+"/nl/residences/chatel/"=>"https://www.chalet.nl/wintersport/plaats/Chatel",
+"/nl/residences/chinaillon/"=>"https://www.chalet.nl/zoek-en-boek.php",
+"/nl/residences/flaine/"=>"https://www.chalet.nl/wintersport/plaats/Flaine",
+"/nl/residences/font-romeu/"=>"https://www.chalet.nl/zoek-en-boek.php",
+"/nl/residences/la-plagne/"=>"https://www.chalet.nl/wintersport/skigebied/Paradiski-La-Plagne/",
+"/nl/residences/la-rosiere/"=>"https://www.chalet.nl/wintersport/plaats/La-Rosiere/",
+"/nl/residences/la-toussuire/"=>"https://www.chalet.nl/wintersport/plaats/La-Toussuire/",
+"/nl/residences/les-carroz-daraches/"=>"https://www.chalet.nl/wintersport/plaats/Les-Carroz",
+"/nl/residences/les-houches/"=>"https://www.chalet.nl/wintersport/plaats/Les-Houches",
+"/nl/residences/les-menuires/"=>"https://www.chalet.nl/wintersport/plaats/Les-Menuires",
+"/nl/residences/les-saisies/"=>"https://www.chalet.nl/wintersport/plaats/Les-Saisies",
+"/nl/residences/montgenevre/"=>"https://www.chalet.nl/wintersport/plaats/Montgenevre",
+"/nl/residences/oz-en-oisans/"=>"https://www.chalet.nl/wintersport/plaats/Oz-en-Oisans/",
+"/nl/residences/sainte-foy-en-tarentaise/"=>"https://www.chalet.nl/wintersport/plaats/St-Foy",
+"/nl/residences/samoens/"=>"https://www.chalet.nl/wintersport/plaats/Samoens",
+"/nl/residences/serre-chevalier/"=>"https://www.chalet.nl/wintersport/plaats/Serre-Chevalier-1350-Chantemerle/",
+"/nl/residences/saint-martin-de-belleville/"=>"https://www.chalet.nl/wintersport/plaats/Saint-Martin-de-Belleville",
+"/nl/residences/tignes/"=>"https://www.chalet.nl/wintersport/plaats/Tignes",
+"/nl/residences/val-cenis-/"=>"https://www.chalet.nl/wintersport/plaats/Val-Cenis",
+"/nl/residences/vallandry/"=>"https://www.chalet.nl/wintersport/plaats/Vallandry",
+"/nl/residences/valloire/"=>"https://www.chalet.nl/wintersport/plaats/Valloire",
+"/nl/residences/skigebied/domaine-le-grand-serre-che/"=>"https://www.chalet.nl/zoek-en-boek.php",
+"/nl/residences/skigebied/espace-diamant/"=>"https://www.chalet.nl/wintersport/skigebied/Espace-Diamant/",
+"/nl/residences/skigebied/espace-killy/"=>"https://www.chalet.nl/wintersport/skigebied/Espace-Killy-Tignes/",
+"/nl/residences/skigebied/espace-san-bernardo/"=>"https://www.chalet.nl/wintersport/skigebied/La-Rosiere-Espace-San-Bernardo/",
+"/nl/residences/skigebied/evasion-mont-blanc/"=>"https://www.chalet.nl/zoek-en-boek.php",
+"/nl/residences/skigebied/grandes-rousses/"=>"https://www.chalet.nl/zoek-en-boek.php",
+"/nl/residences/skigebied/le-grand-bornand/"=>"https://www.chalet.nl/wintersport/plaats/Le-Grand-Bornand-Chinaillon/",
+"/nl/residences/skigebied/le-grand-massif/"=>"https://www.chalet.nl/wintersport/skigebied/Le-Grand-Massif/",
+"/nl/residences/skigebied/les-portes-du-soleil/"=>"https://www.chalet.nl/wintersport/skigebied/Les-Portes-du-Soleil/",
+"/nl/residences/skigebied/les-sybelles/"=>"https://www.chalet.nl/wintersport/skigebied/Les-Sybelles/",
+"/nl/residences/skigebied/les-trois-valles/"=>"https://www.chalet.nl/wintersport/skigebied/Les-Trois-Vallees/",
+"/nl/residences/skigebied/massif-de-montblanc/"=>"https://www.chalet.nl/zoek-en-boek.php",
+"/nl/residences/skigebied/sainte-foy-en-tarentaise/"=>"https://www.chalet.nl/zoek-en-boek.php",
+"/nl/residences/skigebied/pyrnes-2000/"=>"https://www.chalet.nl/zoek-en-boek.php",
+"/nl/residences/skigebied/paradiski/"=>"https://www.chalet.nl/wintersport/skigebied/Paradiski-Les-Arcs/",
+"/nl/residences/skigebied/val-cenis/"=>"https://www.chalet.nl/wintersport/plaats/Val-Cenis/",
+"/nl/residences/skigebied/valmeiniervalloire/"=>"https://www.chalet.nl/wintersport/plaats/Valmeinier/",
+"/nl/residences/skigebied/via-lattea/"=>"https://www.chalet.nl/wintersport/skigebied/Via-Lattea/",
+"/nl/chaletpark/beschikbaarheid/6kamer-chalet-10-personen/"=>"https://www.chalet.nl/zoek-en-boek.php?scrolly=0&filled=1&fsg=&fsg_invoer=&fad=0&fap=0&fas=5&fzt=Hameau+des+Marmottes&lev=0",
+"/nl/chaletpark/beschikbaarheid/7kamer-chalet-12-personen-in-les-menuires/"=>"https://www.chalet.nl/zoek-en-boek.php?scrolly=0&filled=1&fsg=&fsg_invoer=&fad=0&fap=0&fas=6&fzt=Hameau+des+Marmottes&lev=0",
+"/nl/chaletpark/beschikbaarheid/8kamer-chalet-14-personen/"=>"https://www.chalet.nl/zoek-en-boek.php?scrolly=100&filled=1&fsg=&fsg_invoer=&fad=0&fap=0&fas=7&fzt=Hameau+des+Marmottes&lev=0",
+"/nl/residences/le-hameau-des-marmottes/beschikbaarheid/3kamer-appartement-voor-4-tot-6-personen/"=>"https://www.chalet.nl/accommodatie/F2/",
+"/nl/residences/le-hameau-des-marmottes/beschikbaarheid/4kamer-appartement-voor-6-tot-8-personen/"=>"https://www.chalet.nl/accommodatie/F7/",
+"/nl/residences/le-hameau-des-marmottes/beschikbaarheid/5kamer-appartement-voor-8-tot-10-personen/"=>"https://www.chalet.nl/zoek-en-boek.php?scrolly=0&filled=1&fsg=&fsg_invoer=&fad=0&fap=0&fas=5&fzt=Hameau+des+Marmottes&lev=0",
+
+"/eng/"=>"https://www.chalet.eu/",
+"/eng/chaletpark/"=>"https://www.chalet.eu/search-and-book.php?filled=1&referer=2&selb=0&fad=&fap=&fzt=Hameau+des+Marmottes",
+"/eng/residences/"=>"https://www.chalet.eu/country/France/",
+"/eng/residences/bourg-saint-maurice/"=>"https://www.chalet.eu/winter-sports/resort/Bourg-Saint-Maurice/",
+"/eng/residences/champagny/"=>"https://www.chalet.eu/winter-sports/resort/Champagny-en-Vanoise/",
+"/eng/residences/chatel/"=>"https://www.chalet.eu/winter-sports/resort/Chatel",
+"/eng/residences/chinaillon/"=>"https://www.chalet.eu/search-and-book.php",
+"/eng/residences/flaine/"=>"https://www.chalet.eu/winter-sports/resort/Flaine/",
+"/eng/residences/font-romeu/"=>"https://www.chalet.eu/search-and-book.php",
+"/eng/residences/la-plagne/"=>"https://www.chalet.eu/winter-sports/resort/La-Plagne",
+"/eng/residences/la-rosiere/"=>"https://www.chalet.eu/winter-sports/resort/La-Rosiere/",
+"/eng/residences/la-toussuire/"=>"https://www.chalet.eu/winter-sports/resort/La-Toussuire/",
+"/eng/residences/les-carroz-daraches/"=>"https://www.chalet.eu/winter-sports/resort/Les-Carroz/",
+"/eng/residences/les-houches/"=>"https://www.chalet.eu/winter-sports/resort/Les-Houches/",
+"/eng/residences/les-menuires/"=>"https://www.chalet.eu/winter-sports/resort/Les-Menuires/",
+"/eng/residences/les-saisies/"=>"https://www.chalet.eu/winter-sports/resort/Les-Saisies/",
+"/eng/residences/montgenevre/"=>"https://www.chalet.eu/search-and-book.php",
+"/eng/residences/oz-en-oisans/"=>"https://www.chalet.eu/winter-sports/resort/Oz-en-Oisans/",
+"/eng/residences/sainte-foy-en-tarentaise/"=>"https://www.chalet.eu/winter-sports/resort/St-Foy/",
+"/eng/residences/samoens/"=>"https://www.chalet.eu/winter-sports/resort/Samoens",
+"/eng/residences/serre-chevalier/"=>"https://www.chalet.eu/winter-sports/resort/Serre-Chevalier-1350-Chantemerle/",
+"/eng/residences/saint-martin-de-belleville/"=>"https://www.chalet.eu/winter-sports/resort/Saint-Martin-de-Belleville/",
+"/eng/residences/tignes/"=>"https://www.chalet.eu/winter-sports/resort/Tignes/",
+"/eng/residences/val-cenis-/"=>"https://www.chalet.eu/winter-sports/resort/Val-Cenis/",
+"/eng/residences/vallandry/"=>"https://www.chalet.eu/winter-sports/resort/Vallandry",
+"/eng/residences/valloire/"=>"https://www.chalet.eu/winter-sports/resort/Valloire",
+"/eng/residences/skigebied/domaine-le-grand-serre-che/"=>"https://www.chalet.eu/search-and-book.php",
+"/eng/residences/skigebied/espace-diamant/"=>"https://www.chalet.eu/winter-sports/region/Espace-Diamant/",
+"/eng/residences/skigebied/espace-killy/"=>"https://www.chalet.eu/winter-sports/region/Espace-Killy-Tignes/",
+"/eng/residences/skigebied/espace-san-bernardo/"=>"https://www.chalet.eu/winter-sports/region/La-Rosiere-Espace-San-Bernardo/",
+"/eng/residences/skigebied/evasion-mont-blanc/"=>"https://www.chalet.eu/search-and-book.php",
+"/eng/residences/skigebied/grandes-rousses/"=>"https://www.chalet.eu/search-and-book.php",
+"/eng/residences/skigebied/le-grand-bornand/"=>"https://www.chalet.eu/winter-sports/resort/Le-Grand-Bornand-Chinaillon/",
+"/eng/residences/skigebied/le-grand-massif/"=>"https://www.chalet.eu/winter-sports/region/Le-Grand-Massif/",
+"/eng/residences/skigebied/les-portes-du-soleil/"=>"https://www.chalet.eu/winter-sports/region/Les-Portes-du-Soleil/",
+"/eng/residences/skigebied/les-sybelles/"=>"https://www.chalet.eu/winter-sports/region/Les-Sybelles/",
+"/eng/residences/skigebied/les-trois-valles/"=>"https://www.chalet.eu/winter-sports/region/Les-Trois-Vallees/",
+"/eng/residences/skigebied/massif-de-montblanc/"=>"https://www.chalet.eu/search-and-book.php",
+"/eng/residences/skigebied/sainte-foy-en-tarentaise/"=>"https://www.chalet.eu/search-and-book.php",
+"/eng/residences/skigebied/pyrnes-2000/"=>"https://www.chalet.eu/search-and-book.php",
+"/eng/residences/skigebied/paradiski/"=>"https://www.chalet.eu/winter-sports/region/Paradiski-Les-Arcs/",
+"/eng/residences/skigebied/val-cenis/"=>"https://www.chalet.eu/winter-sports/resort/Val-Cenis/",
+"/eng/residences/skigebied/valmeiniervalloire/"=>"https://www.chalet.eu/winter-sports/resort/Valmeinier/",
+"/eng/residences/skigebied/via-lattea/"=>"https://www.chalet.eu/winter-sports/region/Via-Lattea/",
+"/eng/chaletpark/beschikbaarheid/6kamer-chalet-10-personen/"=>"https://www.chalet.eu/search-and-book.php?scrolly=0&filled=1&fsg=&fsg_invoer=&fad=0&fap=0&fas=5&fzt=Hameau+des+Marmottes",
+"/eng/chaletpark/beschikbaarheid/7kamer-chalet-12-personen-in-les-menuires/"=>"https://www.chalet.eu/search-and-book.php?scrolly=0&filled=1&fsg=&fsg_invoer=&fad=0&fap=0&fas=6&fzt=Hameau+des+Marmottes",
+"/eng/chaletpark/beschikbaarheid/8kamer-chalet-14-personen/"=>"https://www.chalet.eu/search-and-book.php?scrolly=0&filled=1&fsg=&fsg_invoer=&fad=0&fap=0&fas=7&fzt=Hameau+des+Marmottes",
+"/eng/residences/le-hameau-des-marmottes/beschikbaarheid/3kamer-appartement-voor-4-tot-6-personen/"=>"https://www.chalet.eu/accommodation/F2/",
+"/eng/residences/le-hameau-des-marmottes/beschikbaarheid/4kamer-appartement-voor-6-tot-8-personen/"=>"https://www.chalet.eu/accommodation/F7/",
+"/eng/residences/le-hameau-des-marmottes/beschikbaarheid/5kamer-appartement-voor-8-tot-10-personen/"=>"https://www.chalet.eu/search-and-book.php?scrolly=0&filled=1&fsg=&fsg_invoer=&fad=0&fap=0&fas=5&fzt=Hameau+des+Marmottes"
+
+);
+
+
+
+$check_url = preg_replace("@\?.*@", "", $_SERVER["REQUEST_URI"]);
+foreach ($redirect as $key => $value) {
+	if($key==$check_url) {
+		$new_url = $value;
+	}
+
+	if(substr($key,0,-1)==$check_url) {
+		$new_url = $value;
+	}
+
+}
+
+
+if(!$new_url) {
+	if(preg_match("@/eng/@", $_SERVER["REQUEST_URI"])) {
+		$new_url="https://www.chalet.eu/";
+	} else {
+		$new_url="https://www.chalet.nl/";
+	}
+}
+
+// echo "GOTO:".$new_url;
+header("Location: ".$new_url, true, 301);
+
+
+?>
