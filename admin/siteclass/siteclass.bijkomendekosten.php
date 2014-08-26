5a7b10f37de5cb4dd01c06ac04118ec03f4fc40a
Jeroen Boschman <jeroen@webtastic.nl>
2014-08-26 17:46:07 +0200

    first setup bijkomende kosten

---
+++ admin/siteclass/siteclass.bijkomendekosten.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+* Bijkomende kosten
+*/
+
+class bijkomendekosten {
+
+	function __construct($id, $type=true) {
+
+		$db = new DB_sql;
+
+		// // get data from database
+		// if($type) {
+		// 	$db->query("SELECT ")
+		// } else {
+
+		// }
+
+	}
+
+	public function cms_all_rows() {
+
+	}
+
+
+	public function cms_new_row($bk_soort_id) {
+
+	}
+
+
+
+}
+
+
+?>
