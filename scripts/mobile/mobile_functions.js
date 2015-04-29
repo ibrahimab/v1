47743480b61fa93521fbbfafd7275d1e8195b644
Jeroen Boschman <jeroen@webtastic.nl>
2015-04-29 16:15:35 +0200

    [CMS-27] bugfix: correctly switch bkk-seasons below pricetable

--- scripts/mobile/mobile_functions.js
+++ scripts/mobile/mobile_functions.js
@@ -14,11 +14,11 @@ function bindCountriesDialCodes() {
 
 $(function(){
 
-        $(window).resize(function(){
-            $("#fancybox-wrap").width("auto");
-            $("#fancybox-content").width("auto");
-        });
-        
+		$(window).resize(function(){
+			$("#fancybox-wrap").width("auto");
+			$("#fancybox-content").width("auto");
+		});
+
 	//var tarieventabel_pijl_boven = $(".tarieventabel_pijl_boven").clone();
 	//var tarieventabel_pijl_onder = $(".tarieventabel_pijl_onder").clone();
 	////$(".tarieventabel_pijl_boven").remove();
@@ -44,20 +44,20 @@ $(function(){
 		ajax : {
 			type	: "GET"
 		},
-                'onComplete'	: function() {
-                        $.fancybox.resize();
+				'onComplete'	: function() {
+						$.fancybox.resize();
 		},
 	});
 
 	// iOS 7 fix: Add an optgroup to every select in order to avoid truncating the content
 	if (navigator.userAgent.match(/(iPad|iPhone|iPod touch);.*CPU.*OS 7_\d/i)) {
-	    var selects = document.querySelectorAll("select");
-	    for (var i = 0; i < selects.length; i++ ){
-	        selects[i].appendChild(document.createElement("optgroup"));
-	    }
-	}    
-	     
-        $(".popwindow-iframe").fancybox({
+		var selects = document.querySelectorAll("select");
+		for (var i = 0; i < selects.length; i++ ){
+			selects[i].appendChild(document.createElement("optgroup"));
+		}
+	}
+
+		$(".popwindow-iframe").fancybox({
 		type		: 'iframe',
 		width		: '100%',
 		height		: '100%',
@@ -67,10 +67,10 @@ $(function(){
 		closeEffect	: 'none',
 		margin		: 20,
 		scrolling	: 'yes',
-                'onComplete'	: function() {
-                	$.fancybox.resize();
+				'onComplete'	: function() {
+					$.fancybox.resize();
 		},
-		
+
 	});
 
 	$('div#menu-left').mmenu({classes: "mm-light"});
@@ -88,50 +88,50 @@ $(function(){
 		},
 		btnNext: function() {
 			return $(this).find('.next');
-		},               
+		},
 	};
 
 $(".slideshow").jCarouselLite(carouselOptions);
-    
+
 	if($('#accordion').length > 0 && $().tabs) {
 		$('#accordion').accordion({
 			collapsible: true,
 			autoHeight: false,
 			active: false,
-                        heightStyle: "content",
-                        beforeActivate: function(event, ui) {
-                            // The accordion believes a panel is being opened
-                           if (ui.newHeader[0]) {
-                               var currHeader  = ui.newHeader;
-                               var currContent = currHeader.next('.ui-accordion-content');
-                            // The accordion believes a panel is being closed
-                           } else {
-                               var currHeader  = ui.oldHeader;
-                               var currContent = currHeader.next('.ui-accordion-content');
-                           }
-                            // Since we've changed the default behavior, this detects the actual status
-                           var isPanelSelected = currHeader.attr('aria-selected') == 'true';
+						heightStyle: "content",
+						beforeActivate: function(event, ui) {
+							// The accordion believes a panel is being opened
+						   if (ui.newHeader[0]) {
+							   var currHeader  = ui.newHeader;
+							   var currContent = currHeader.next('.ui-accordion-content');
+							// The accordion believes a panel is being closed
+						   } else {
+							   var currHeader  = ui.oldHeader;
+							   var currContent = currHeader.next('.ui-accordion-content');
+						   }
+							// Since we've changed the default behavior, this detects the actual status
+						   var isPanelSelected = currHeader.attr('aria-selected') == 'true';
 
-                            // Toggle the panel's header
-                           currHeader.toggleClass('ui-corner-all',isPanelSelected).toggleClass('accordion-header-active ui-state-active ui-corner-top',!isPanelSelected).attr('aria-selected',((!isPanelSelected).toString()));
+							// Toggle the panel's header
+						   currHeader.toggleClass('ui-corner-all',isPanelSelected).toggleClass('accordion-header-active ui-state-active ui-corner-top',!isPanelSelected).attr('aria-selected',((!isPanelSelected).toString()));
 
-                           // Toggle the panel's icon
-                           currHeader.children('.ui-icon').toggleClass('ui-icon-triangle-1-e',isPanelSelected).toggleClass('ui-icon-triangle-1-s',!isPanelSelected);
+						   // Toggle the panel's icon
+						   currHeader.children('.ui-icon').toggleClass('ui-icon-triangle-1-e',isPanelSelected).toggleClass('ui-icon-triangle-1-s',!isPanelSelected);
 
-                            // Toggle the panel's content
-                           currContent.toggleClass('accordion-content-active',!isPanelSelected); 
-                           if (isPanelSelected) { currContent.slideUp(); }  else { currContent.slideDown(); }
+							// Toggle the panel's content
+						   currContent.toggleClass('accordion-content-active',!isPanelSelected);
+						   if (isPanelSelected) { currContent.slideUp(); }  else { currContent.slideDown(); }
 
-                                var scrollTop = $(".accordion").scrollTop();
-                                if(ui.newHeader.offset())
-                                    var top = $(ui.newHeader).offset().top;
-                                else 
-                                    var top = ui.oldHeader.offset().top;
-                             //do magic to scroll the user to the correct location
+								var scrollTop = $(".accordion").scrollTop();
+								if(ui.newHeader.offset())
+									var top = $(ui.newHeader).offset().top;
+								else
+									var top = ui.oldHeader.offset().top;
+							 //do magic to scroll the user to the correct location
 
-                             //works in IE, firefox chrome and safari
-                                $("html,body").animate({ scrollTop: scrollTop + top -35 }, "slow");
-                            // 2 = ligging accordion tab
+							 //works in IE, firefox chrome and safari
+								$("html,body").animate({ scrollTop: scrollTop + top -35 }, "slow");
+							// 2 = ligging accordion tab
 				//if(ui.options.active == 2) {
 					//we have to set center for map after resize, but we need to know center BEFORE we resize it
 				//google.maps.event.trigger(map, "resize"); //this fix the problem with not completely map
@@ -140,19 +140,21 @@ $(".slideshow").jCarouselLite(carouselOptions);
 					map.setCenter(new google.maps.LatLng(mapOptions.googlemaps_lat,mapOptions.googlemaps_long));
 				//}
 
-                           return false; // Cancels the default action
-                        },
+						   return false; // Cancels the default action
+						},
 
 			change: function( event, ui ) {
-                                var scrollTop = $(".accordion").scrollTop();
-                                if(ui.newHeader.offset())
-                                    var top = $(ui.newHeader).offset().top;
-                                else 
-                                    var top = ui.oldHeader.offset().top;
-                                //do magic to scroll the user to the correct location
-                                //works in IE, firefox chrome and safari
-                                $("html,body").animate({ scrollTop: scrollTop + top -35 }, "slow");
-                                // 2 = ligging accordion tab
+								var scrollTop = $(".accordion").scrollTop();
+								if(ui.newHeader.offset())
+									var top = $(ui.newHeader).offset().top;
+								else
+									var top = ui.oldHeader.offset().top;
+								//do magic to scroll the user to the correct location
+								//works in IE, firefox chrome and safari
+								$("html,body").animate({ scrollTop: scrollTop + top -35 }, "slow", function(){
+									tarieventabel_toelichting_check_season();
+								});
+								// 2 = ligging accordion tab
 				if(ui.options.active == 2) {
 					//we have to set center for map after resize, but we need to know center BEFORE we resize it
 				//google.maps.event.trigger(map, "resize"); //this fix the problem with not completely map
@@ -166,28 +168,28 @@ $(".slideshow").jCarouselLite(carouselOptions);
 
 		});
 	}
-        $("a[href*='#extraopties']").click(function() {
-            if($("#extraopties").hasClass("accordion-content-active") == false)
-                $( "#accordion" ).accordion({ active: 3 });
-            
-        });
-        
-        $("a[href*='#prijsinformatie']").click(function() {
-            if($("#prijsinformatie").hasClass("accordion-content-active") == false){
-                var accordionID = parseInt(jQuery("h3[aria-controls='prijsinformatie']").attr("id").split("-")[4]);
-                $( "#accordion" ).accordion({ active: accordionID });
-            }
-        });
-        
-        back_url=location.href;
-        back_url = back_url.replace(location.hash,"");
-        if(back_url.indexOf('toonflex=') > -1) {
-            if($("#prijsinformatie").hasClass("accordion-content-active") == false){
-                var accordionID = parseInt(jQuery("h3[aria-controls='prijsinformatie']").attr("id").split("-")[4]);
-                $( "#accordion" ).accordion({ active: accordionID });
-            }        
-        }
-        
+		$("a[href*='#extraopties']").click(function() {
+			if($("#extraopties").hasClass("accordion-content-active") == false)
+				$( "#accordion" ).accordion({ active: 3 });
+
+		});
+
+		$("a[href*='#prijsinformatie']").click(function() {
+			if($("#prijsinformatie").hasClass("accordion-content-active") == false){
+				var accordionID = parseInt(jQuery("h3[aria-controls='prijsinformatie']").attr("id").split("-")[4]);
+				$( "#accordion" ).accordion({ active: accordionID });
+			}
+		});
+
+		back_url=location.href;
+		back_url = back_url.replace(location.hash,"");
+		if(back_url.indexOf('toonflex=') > -1) {
+			if($("#prijsinformatie").hasClass("accordion-content-active") == false){
+				var accordionID = parseInt(jQuery("h3[aria-controls='prijsinformatie']").attr("id").split("-")[4]);
+				$( "#accordion" ).accordion({ active: accordionID });
+			}
+		}
+
 	$("#verfijn #verfijntopheader").click(function(){
 		$("#verfijntopheader + div").toggle();
 		$("#verfijntopheader span").toggleClass("up");
