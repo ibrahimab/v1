d02f3e638c2654f4b0455da2d1bedaa988ec9e82
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2015-05-15 14:44:30 +0200

    Adding MongoDB constants to production server conf

--- admin/serverconf/define_constants-backup.php
+++ admin/serverconf/define_constants-backup.php
@@ -11,4 +11,5 @@ define("wt_server_id", 0);
 // redis-server
 define("wt_redis_host", "localhost");
 
-?>
+define('CH_MONGODB_MASTER', 'mongodb://localhost:27017');
+define('CH_MONGODB_SLAVE', 'mongodb://localhost:27017');
