ad0b417d8d291f86e9aaa3db1c21a605bf74e69f
Jeroen Boschman <jeroen@webtastic.nl>
2015-05-19 13:51:14 +0200

    [CMS-93] disable WebTastic-mailproxy for Microsoft-emails

--- admin/serverconf/define_constants-web01.php
+++ admin/serverconf/define_constants-web01.php
@@ -11,9 +11,11 @@ define("wt_server_id", 1);
 // redis-server
 define("wt_redis_host", "87.250.157.203");
 
+
+// MS-mailproxy disabled on 19 May 2015 (requested by Wouter)
 // always use WebTastic-mailproxy for Microsoft-mail
-define("wt_mail_proxy_microsoftmail_always", true);
-define("wt_mail_proxy_microsoftmail_copy", "testchalet@outlook.com");
+// define("wt_mail_proxy_microsoftmail_always", true);
+// define("wt_mail_proxy_microsoftmail_copy", "testchalet@outlook.com");
 
 define('CH_MONGODB_MASTER', 'mongodb://87.250.157.214:27017');
 define('CH_MONGODB_SLAVE', 'mongodb://87.250.157.200:27017');
