9314d5afb113cc1f67bcf2cbe06803d0860b7b3e
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2015-08-17 09:47:32 +0200

    [CMS-105] only get big main images

--- admin/siteclass/siteclass.MongoWrapper.php
+++ admin/siteclass/siteclass.MongoWrapper.php
@@ -7,42 +7,42 @@
  */
 class MongoWrapper
 {
-	protected $mongodb;
-	protected $db;
-	protected $error;
+    protected $mongodb;
+    protected $db;
+    protected $error;
 
-	public function __construct($server, $database, $replicaSet = false)
-	{
-		try {
-			
-			$defaultOptions    = ['connect' => true];
-			$replicaSetOptions = ['connect' => true, 'replicaSet' => $replicaSet];
-			$this->mongodb     = new MongoClient($server, (false === $replicaSet ? $defaultOptions : $replicaSetOptions));
-			$this->db          = $this->mongodb->{$database};
-			
-		} catch (\Exception $e) {
-			$this->error = true;
-		}
-	}
+    public function __construct($server, $database, $replicaSet = false)
+    {
+        try {
+            
+            $defaultOptions    = ['connect' => true];
+            $replicaSetOptions = ['connect' => true, 'replicaSet' => $replicaSet];
+            $this->mongodb     = new MongoClient($server, (false === $replicaSet ? $defaultOptions : $replicaSetOptions));
+            $this->db          = $this->mongodb->{$database};
+            
+        } catch (\Exception $e) {
+            $this->error = true;
+        }
+    }
 
-	public function getFiles($collectionName, $fileId)
-	{
-		if (true === $this->error) {
-			return [];
-		}
-		
-		$collection = $this->db->{$collectionName};
+    public function getFiles($collectionName, $fileId)
+    {
+        if (true === $this->error) {
+            return [];
+        }
+        
+        $collection = $this->db->{$collectionName};
 
-		return $collection->find(['file_id' => intval($fileId)])
-						  ->sort(['rank'    => 1]);
-	}
+        return $collection->find(['file_id' => intval($fileId)])
+                          ->sort(['rank'    => 1]);
+    }
 
     public function getFirstFilesByKind($collectionName, $fileIds, $kinds)
     {
-		if (true === $this->error) {
-			return [];
-		}
-		
+        if (true === $this->error) {
+            return [];
+        }
+        
         $collection = $this->db->{$collectionName};
 
         return $collection->find(['file_id' => ['$in' => $fileIds],
@@ -52,10 +52,10 @@ class MongoWrapper
 
     public function getAllFilesByKind($collectionName, $fileIds, $kinds)
     {
-		if (true === $this->error) {
-			return [];
-		}
-		
+        if (true === $this->error) {
+            return [];
+        }
+        
         $collection = $this->db->{$collectionName};
 
         return $collection->find(['file_id' => ['$in' => $fileIds],
@@ -65,10 +65,10 @@ class MongoWrapper
 
     public function getAllFiles($collectionName, $fileIds)
     {
-		if (true === $this->error) {
-			return [];
-		}
-		
+        if (true === $this->error) {
+            return [];
+        }
+        
         $collection = $this->db->{$collectionName};
 
         return $collection->find(['file_id' => ['$in' => $fileIds]])
@@ -77,83 +77,83 @@ class MongoWrapper
 
     public function getAllMainFiles($collectionName, $fileIds)
     {
-		if (true === $this->error) {
-			return [];
-		}
-		
+        if (true === $this->error) {
+            return [];
+        }
+        
         $collection = $this->db->{$collectionName};
 
         return $collection->find(['file_id' => ['$in' => $fileIds],
-                                  'type'    => ['$exists' => true]])
+                                  'type'    => 'big'])
                           ->sort(['rank'    => 1]);
     }
 
-	public function getFile($collectionName, $_id)
-	{
-		if (true === $this->error) {
-			return [];
-		}
-		
-		$collection = $this->db->{$collectionName};
+    public function getFile($collectionName, $_id)
+    {
+        if (true === $this->error) {
+            return [];
+        }
+        
+        $collection = $this->db->{$collectionName};
 
-		return $collection->findOne(['_id' => new MongoId($_id)]);
-	}
+        return $collection->findOne(['_id' => new MongoId($_id)]);
+    }
 
-	public function maxRank($collectionName, $fileId)
-	{
-		if (true === $this->error) {
-			return 0;
-		}
-		
-		$collection = $this->db->{$collectionName};
-		$results    = $collection->aggregate([
+    public function maxRank($collectionName, $fileId)
+    {
+        if (true === $this->error) {
+            return 0;
+        }
+        
+        $collection = $this->db->{$collectionName};
+        $results    = $collection->aggregate([
 
-			[
-				'$match' => [
-					'file_id' => intval($fileId)
-				]
-			],
-			[
-				'$group' => [
-					'_id'  => 'maxRank',
-					'rank' => [
-						'$max' => '$rank',
-					]
-				]
-			]
-		]);
+            [
+                '$match' => [
+                    'file_id' => intval($fileId)
+                ]
+            ],
+            [
+                '$group' => [
+                    '_id'  => 'maxRank',
+                    'rank' => [
+                        '$max' => '$rank',
+                    ]
+                ]
+            ]
+        ]);
 
-		$maxRank = 0;
-		if (isset($results['result']) && isset($results['result'][0])) {
-			$maxRank = $results['result'][0]['rank'];
-		}
+        $maxRank = 0;
+        if (isset($results['result']) && isset($results['result'][0])) {
+            $maxRank = $results['result'][0]['rank'];
+        }
 
-		return $maxRank;
-	}
+        return $maxRank;
+    }
 
-	public function updateFileId($collectionName, $_id, $fileId)
-	{
-		$collection = $this->db->{$collectionName};
+    public function updateFileId($collectionName, $_id, $fileId)
+    {
+        $collection = $this->db->{$collectionName};
 
-		return $collection->update(['_id' => new MongoId($_id)], ['$set' => ['file_id' => $fileId]]);
-	}
+        return $collection->update(['_id' => new MongoId($_id)], ['$set' => ['file_id' => $fileId]]);
+    }
 
-	public function removeMetadata($collectionName, $_id)
-	{
-		$collection = $this->db->{$collectionName};
+    public function removeMetadata($collectionName, $_id)
+    {
+        $collection = $this->db->{$collectionName};
 
-		return $collection->remove(['_id' => new MongoId($_id)]);
-	}
+        return $collection->remove(['_id' => new MongoId($_id)]);
+    }
 
-	public function getBulkUpdater($collectionName)
-	{
-		$collection = $this->db->{$collectionName};
+    public function getBulkUpdater($collectionName)
+    {
+        $collection = $this->db->{$collectionName};
 
-		return new MongoUpdateBatch($collection);
-	}
+        return new MongoUpdateBatch($collection);
+    }
 
-	public function getCollection($collectionName)
-	{
-		return $this->db->{$collectionName};
-	}
+    public function getCollection($collectionName)
+    {
+        return $this->db->{$collectionName};
+    }
 }
