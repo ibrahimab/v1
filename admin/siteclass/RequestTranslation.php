d443a569f925e5de3fdb708f88f29f818ff788f1
Eric Minang <minangeric@gmail.com>
2015-11-03 13:05:33 +0100

    [CMS-170]
    * create 2tables on same page using new class

--- admin/siteclass/RequestTranslation.php
+++ admin/siteclass/RequestTranslation.php
@@ -16,6 +16,16 @@ class RequestTranslation
 	private $config;
 
 	/**
+	 * @var integer
+	 */
+	private $season;
+
+	public function __construct($season)
+	{
+		$this->season = (int)$season;
+	}
+
+	/**
 	 *
 	 */
 	public function setConfiguration($config)
@@ -23,17 +33,60 @@ class RequestTranslation
 		$this->config = $config;
 	}
 
+
 	public function accommodations()
 	{
+		$db = $this->getDatabase();
+
+		$db->query("SELECT a.accommodatie_id , a.naam AS accommodatie, a.wzt, a.naam, p.plaats_id, p.naam AS plaats,
+		           		   l.leverancier_id, l.naam AS leverancier
+					FROM    accommodatie a, plaats p, leverancier l
+					WHERE 	a.leverancier_id = l.leverancier_id
+					AND     a.plaats_id = p.plaats_id
+					AND     a.wzt = " . $this->season . "
+					AND     a.request_translation" . $this->config->ttv . " = 1");
+
+		if ($db->num_rows()) {
+
+			$tl = new tablelist;
+			$tl->settings['systemid']   = 2;
+			$tl->settings['arrowcolor'] = 'white';
+			$tl->settings['path']		= $this->config->path;
+			$tl->settings['th_id']		= 'col_';
+			$tl->settings['td_class']	= 'col_';
+
+			$tl->sort 					= ['naam'];
+			$tl->sort_desc				= true;
+
+			// which fields to show in table
+			$tl->field_show('cms_types.php?show=2&bc=630&wzt=1&archief=0&1k0=&2k0=[ID]', 'Details bekijken');
+			$tl->field_text('leverancier', 'Leverancier');
+			$tl->field_text('plaats_id', 'Plaats');
+			$tl->field_text('naam', 'Interne Naam');
+
+			while ($db->next_record()) {
+
+				// add_record($id,$key,$value,$sortvalue="",$datetime=false,$options="")
+				$tl->add_record('naam', $db->f('accommodatie_id'), $db->f('accommodatie') . ' ' . $db->f('naam'));
+				$tl->add_record('plaats_id', $db->f('accommodatie_id'), $db->f('plaats'));
+				$tl->add_record('leverancier', $db->f('accommodatie_id'), $db->f('leverancier'));
+			}
+
+			return $tl;
+		}
+
+		return false;
+
 	}
 
 	public function types()
 	{
 		$db = $this->getDatabase();
-		$db->query("SELECT t.type_id , a.naam AS accommodatie, t.naam, p.plaats_id, p.naam AS plaats,
+		$db->query("SELECT t.type_id , a.naam AS accommodatie, a.wzt, t.naam, p.plaats_id, p.naam AS plaats,
 		           		   l.leverancier_id, l.naam AS leverancier
 					FROM type t, accommodatie a, plaats p, leverancier l
 					WHERE t.accommodatie_id = a.accommodatie_id
+					AND   a.wzt = " . $this->season. "
 					AND t.leverancier_id= l.leverancier_id
 					AND a.plaats_id = p.plaats_id
 					AND t.request_translation" . $this->config->ttv . " = 1");
