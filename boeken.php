1b72030918b386918075aacc4bf4b3c2a1c18062
Eric Minang <minangeric@gmail.com>
2016-01-06 15:15:41 +0100

    [CMS-261]
    *  added one row voorraad on booking mail form to customer

--- boeken.php
+++ boeken.php
@@ -3005,6 +3005,21 @@ if($mustlogin or $boeking_wijzigen or ($accinfo["tonen"] and !$niet_beschikbaar)
 			}
 
 			$tabellen.="<tr><td style=\"width:165px;font-weight: bold;border:solid ".$table." 1px\">".html("aanvraagnummer","boeken")."</td><td style=\"border:solid ".$table." 1px\">[[aanvraagnummer_link_open]]".$gegevens["stap1"]["boekingid"]."[[aanvraagnummer_link_sluit]]</td></tr>";
+			 // Voorraad text
+			$db4->query("SELECT voorraad_garantie, voorraad_allotment, voorraad_vervallen_allotment, voorraad_xml, voorraad_request FROM tarief WHERE type_id='".addslashes($accinfo["type_id"])."' AND week='".addslashes($gegevens["stap1"]["aankomstdatum_exact"])."';");
+			$voorraadweergave = [];
+			if($db4->next_record()) {
+				if($db4->f("voorraad_garantie")>0) $voorraadweergave[] = $db4->f("voorraad_garantie") . 'x garantie';
+				if($db4->f("voorraad_allotment")>0) $voorraadweergave[] = $db4->f("voorraad_allotment") . 'x allotment';
+				if($db4->f("voorraad_vervallen_allotment")>0) $voorraadweergave[] = $db4->f("voorraad_vervallen_allotment") . 'x vervallen allotment';
+				if($db4->f("voorraad_xml")>0) $voorraadweergave[] = $db4->f("voorraad_xml") . 'x xml';
+				if($db4->f("voorraad_request")>0) $voorraadweergave[] = $db4->f("voorraad_request") . 'x request';
+			}
+			$voorraad_row = '';
+			if (count($voorraadweergave) > 0) {
+				$voorraad_row = '<tr><td style="width:165px;font-weight: bold;border:solid ' . $table . ' 1px">' . html('voorraad', 'boeken') . '</td><td style="border:solid ' . $table . ' 1px">' . implode(', ', $voorraadweergave) . '</td></tr>';
+			}
+			$tabellen .= '[[voorraad_row]]';
 			$tabellen.="<tr><td style=\"width:165px;font-weight: bold;border:solid ".$table." 1px\">".html("accommodatie","boeken")."</td><td style=\"border:solid ".$table." 1px\"><a href=\"".$accinfo["url"]."\">".ucfirst($accinfo["soortaccommodatie"])." ".wt_he($accinfo["naam_ap"])."</a></td></tr>";
 			$tabellen.="<tr><td style=\"width:165px;font-weight: bold;border:solid ".$table." 1px\">".html("plaats","boeken")."</td><td style=\"border:solid ".$table." 1px\"><a href=\"".$accinfo["plaats_url"]."\">".wt_he($accinfo["plaats"].", ".$accinfo["land"])."</a></td></tr>";
 			$tabellen.="<tr><td style=\"width:165px;font-weight: bold;border:solid ".$table." 1px\">".html("aantalpersonen","boeken")."</td><td style=\"border:solid ".$table." 1px\">".wt_he($gegevens["stap1"]["aantalpersonen"])."</td></tr>";
@@ -3130,7 +3145,7 @@ if($mustlogin or $boeking_wijzigen or ($accinfo["tonen"] and !$niet_beschikbaar)
 
 			$html="<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=iso-8859-1\"/><style type=\"text/css\"><!--\na:visited:hover,a:hover {\ncolor:".$hover.";\n}\n--></style>\n</head>\n<body style=\"background-color: #F3F3F3;font-family: ".$font."font-size: 0.8em;\">\n";
 			$html.="<div style=\"width:630px\">";
-			$html.="&nbsp;<br>De volgende gegevens zijn zojuist via de website ingevoerd:<p>".str_replace(['[[aanvraagnummer_link_open]]', '[[aanvraagnummer_link_sluit]]'], ['<a href="' . $vars['websiteinfo']['basehref']['C'] . 'cms_boekingen.php?show=21&bt=1&21k0=' . $gegevens["stap1"]["boekingid"] . '">', '</a>'], $tabellen);
+			$html.="&nbsp;<br>De volgende gegevens zijn zojuist via de website ingevoerd:<p>".str_replace(['[[aanvraagnummer_link_open]]', '[[aanvraagnummer_link_sluit]]', '[[voorraad_row]]'], ['<a href="' . $vars['websiteinfo']['basehref']['C'] . 'cms_boekingen.php?show=21&bt=1&21k0=' . $gegevens["stap1"]["boekingid"] . '">', '</a>', $voorraad_row], $tabellen);
 
 			if($referer["opsomming"] or $form->input["referentiekeuze"]) {
 				$html.="<p><table cellspacing=\"0\" cellpadding=\"3\" style=\"background-color: #FFFFFF;width: 630px;font-family: ".$font.";font-size: 1.0em;border:solid ".$table." 1px;\">";
@@ -3152,7 +3167,8 @@ if($mustlogin or $boeking_wijzigen or ($accinfo["tonen"] and !$niet_beschikbaar)
 
 			$html.="</div></body></html>";
 
-			$mail->html=$html;
+			$mail->html = $html;
+
 			if(!$voorkant_cms) {
 				$mail->send();
 			}
@@ -3213,7 +3229,7 @@ if($mustlogin or $boeking_wijzigen or ($accinfo["tonen"] and !$niet_beschikbaar)
 			if(!$gegevens["stap1"]["reisbureau_user_id"]) {
 				$html.="<p>".$inlogtext."</p>";
 			}
-			$html.="<br>".str_replace(['[[aanvraagnummer_link_open]]', '[[aanvraagnummer_link_sluit]]'], '', $tabellen)."<br/>";
+			$html.="<br>".str_replace(['[[aanvraagnummer_link_open]]', '[[aanvraagnummer_link_sluit]]', '[[voorraad_row]]'], '', $tabellen)."<br/>";
 			$html.="<p>".html("metvriendelijkegroet","boeken")."<br>".html("medewerkerssitenaam","boeken",array("v_websitenaam"=>$vars["websitenaam"]))."<P>".$vars["langewebsitenaam"]."<br>Wipmolenlaan 3<br>3447 GJ Woerden<br>".($vars["websiteland"]<>"nl" ? html("nederland","contact")."<br>" : "").html("telefoonnummer_chalet","contact")."<br>".html("fax_chalet","contact")."<br>".html("email_kort","contact").": <a href=\"mailto:".$vars["email"]."\">".$vars["email"]."</a></p><br>&nbsp;";
 
 #			$html.="</div></body></html>";
