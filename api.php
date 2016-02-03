a103db8b5013456e4db5fbea1e162fafe220a2c4
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2016-02-03 17:10:36 +0100

    [DEV-19]
    * added exception handling for api
    * send exception message to client

--- api.php
+++ api.php
@@ -6,11 +6,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 $request  = Request::createFromGlobals();
-$api      = new Api($request);
-$endpoint = $api->getEndpoint();
-$result   = $endpoint->result();
+try {
 
-$response = new JsonResponse();
-$response->setData($result);
+    $api      = new Api($request);
+    $endpoint = $api->getEndpoint();
+    $result   = $endpoint->result();
+
+    $response = new JsonResponse();
+    $response->setData($result);
+
+} catch (\Exception $e) {
+
+    $response = new JsonResponse();
+    $response->setData([
+
+        'type' => 'error',
+        'message' => $e->getMessage(),
+    ]);
+}
 
 return $response->send();
