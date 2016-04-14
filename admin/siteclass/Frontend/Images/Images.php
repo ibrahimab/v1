20eaaf579a5e0b5a21efc2a43086034b124b8d4a
Ibrahim <abdul.ibrahim16@gmail.com>
2016-04-14 17:31:01 +0200

    [feature/res-234]
    * replaced filesystem fetch with Images fetcher
    * added image sizes for small_above and small_below
    * refactored Images class to allow for better fetching of data

--- admin/siteclass/Frontend/Images/Images.php
+++ admin/siteclass/Frontend/Images/Images.php
@@ -11,6 +11,16 @@ use Chalet\Frontend\Images\TypeRepositoryInterface;
 class Images
 {
     /**
+     * @var integer
+     */
+    const KIND_TYPE = 1;
+
+    /**
+     * @var integer
+     */
+    const KIND_ACCOMMODATION = 2;
+
+    /**
      * @var AccommodationRepositoryInterface
      */
     private $accommodationRepository;
@@ -31,6 +41,31 @@ class Images
     private $main;
 
     /**
+     * @var array
+     */
+    private $mainKinds;
+
+    /**
+     * @var array
+     */
+    private $smallAbove;
+
+    /**
+     * @var array
+     */
+    private $smallAboveKinds;
+
+    /**
+     * @var array
+     */
+    private $smallBelow;
+
+    /**
+     * @var array
+     */
+    private $smallBelowKinds;
+
+    /**
      * @var string
      */
     private $default;
@@ -44,6 +79,11 @@ class Images
         $this->accommodationRepository = $accommodationRepository;
         $this->images                  = [];
         $this->main                    = [];
+        $this->mainKinds               = [];
+        $this->smallAbove              = [];
+        $this->smallAboveKinds         = [];
+        $this->smallBelow              = [];
+        $this->smallBelowKinds         = [];
         $this->default                 = [];
     }
 
@@ -70,13 +110,15 @@ class Images
      */
     public function main($typeId, $accommodationId)
     {
-        $main = $this->getMainImage($typeId, $accommodationId);
-
-        if (null === $main) {
-            $main = $this->default;
+        if (!isset($this->main[$typeId])) {
+            $this->defineMainImages($typeId, $accommodationId);
         }
 
-        return $main;
+        if (!isset($this->main[$typeId]) || null === $this->main[$typeId]) {
+            $this->main[$typeId] = $this->default;
+        }
+
+        return $this->main[$typeId];
     }
 
     /**
@@ -85,40 +127,134 @@ class Images
      *
      * @return array|null
      */
-    public function getMainImage($typeId, $accommodationId)
+    public function smallAbove($typeId, $accommodationId)
     {
-        if (!isset($this->main[$typeId])) {
+        if (!isset($this->smallAbove[$typeId])) {
+            $this->defineMainImages($typeId, $accommodationId);
+        }
 
-            $images = $this->typeImages($typeId);
-            $main   = null;
+        if (!isset($this->smallAbove[$typeId]) || null === $this->smallAbove[$typeId]) {
+            $this->smallAbove[$typeId] = $this->default;
+        }
+
+        return $this->smallAbove[$typeId];
+    }
+
+    /**
+     * @param integer $id
+     * @param integer $accommodationId
+     *
+     * @return array|null
+     */
+    public function smallBelow($typeId, $accommodationId)
+    {
+        if (!isset($this->smallBelow[$typeId])) {
+            $this->defineMainImages($typeId, $accommodationId);
+        }
+
+        if (!isset($this->smallBelow[$typeId]) || null === $this->smallBelow[$typeId]) {
+            $this->smallBelow[$typeId] = $this->default;
+        }
+
+        return $this->smallBelow[$typeId];
+    }
+
+    /**
+     * @return array|null
+     */
+    private function defineMainImages($typeId, $accommodationId)
+    {
+        $images     = $this->typeImages($typeId);
+        $main       = null;
+        $smallAbove = null;
+        $smallBelow = null;
+
+        foreach ($images as $image) {
+
+            if (isset($image['type'])) {
+
+                if ($image['type'] === 'big') {
+
+                    $main = $image;
+                    $this->mainKinds[$typeId] = self::KIND_TYPE;
+                }
+
+                if ($image['type'] === 'small_above') {
+
+                    $smallAbove = $image;
+                    $this->smallAboveKinds[$typeId] = self::KIND_TYPE;
+                }
+
+                if ($image['type'] === 'small_below') {
+
+                    $smallBelow = $image;
+                    $this->smallBelowKinds[$typeId] = self::KIND_TYPE;
+                }
+            }
+        }
+
+        if (null === $main || null === $smallAbove || null === $smallBelow) {
+
+            $images = $this->accommodationImages($accommodationId);
 
             foreach ($images as $image) {
 
-                if (isset($image['type']) && $image['type'] === 'big') {
+                if (isset($image['type'])) {
 
-                    $main = $image;
-                    break;
-                }
-            }
-
-            if (null === $main) {
-
-                $images = $this->accommodationImages($accommodationId);
-
-                foreach ($images as $image) {
-
-                    if (isset($image['type']) && $image['type'] === 'big') {
+                    if (null === $main && $image['type'] === 'big') {
 
                         $main = $image;
-                        break;
+                        $this->mainKinds[$typeId] = self::KIND_ACCOMMODATION;
+                    }
+
+                    if (null === $smallAbove && $image['type'] === 'small_above') {
+
+                        $smallAbove = $image;
+                        $this->smallAboveKinds[$typeId] = self::KIND_ACCOMMODATION;
+                    }
+
+                    if (null === $smallBelow && $image['type'] === 'small_below') {
+
+                        $smallBelow = $image;
+                        $this->smallBelowKinds[$typeId] = self::KIND_ACCOMMODATION;
                     }
                 }
             }
-
-            $this->main[$typeId] = $main;
         }
 
-        return $this->main[$typeId];
+        $this->main[$typeId] = $main;
+        $this->smallAbove[$typeId] = $smallAbove;
+        $this->smallBelow[$typeId] = $smallBelow;
+    }
+
+    /**
+     * @param integer $typeId
+     *
+     * @return integer|boolean
+     */
+    public function getMainKind($typeId)
+    {
+        return (isset($this->mainKinds[$typeId]) ? $this->mainKinds[$typeId] : false);
+    }
+
+    /**
+     * @param integer $typeId
+     *
+     * @return integer|boolean
+     */
+    public function getSmallAboveKind($typeId)
+    {
+        return (isset($this->smallAboveKinds[$typeId]) ? $this->smallAboveKinds[$typeId] : false);
+    }
+
+    /**
+     * @param integer $typeId
+     *
+     * @return integer|boolean
+     */
+    public function getSmallBelowKind($typeId)
+    {
+        return (isset($this->smallBelowKinds[$typeId]) ? $this->smallBelowKinds[$typeId] : false);
     }
 
     /**
